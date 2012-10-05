30330dd2bb2440c17ecf170408f438f291227ef4
md_5 <md_5@bigpond.com>
2012-10-05 15:59:14 +1000

    Restrict end to console only.

--- src/main/java/net/md_5/bungee/command/CommandEnd.java
+++ src/main/java/net/md_5/bungee/command/CommandEnd.java
@@ -1,11 +1,15 @@
 package net.md_5.bungee.command;
 
 import net.md_5.bungee.BungeeCord;
+import net.md_5.bungee.ChatColor;
 
 public class CommandEnd extends Command {
 
     @Override
     public void execute(CommandSender sender, String[] args) {
+        if (!(sender instanceof ConsoleCommandSender)) {
+            sender.sendMessage(ChatColor.RED + "Only the console can use this command");
+        }
         BungeeCord.instance.stop();
     }
 }
