e2c357ccb0231c2c460a6c489ca68244ab5e69c2
lol768 <support@technostripe.com>
2012-10-21 22:41:17 +0200

    Allow custom colours and hide prefix

--- src/main/java/net/md_5/bungee/ChatColor.java
+++ src/main/java/net/md_5/bungee/ChatColor.java
@@ -139,15 +139,16 @@ public enum ChatColor
 
         return STRIP_COLOR_PATTERN.matcher(input).replaceAll("");
     }
-    public static String translateAlternateColorCodes(char altColorChar, String textToTranslate) 
+
+    public static String translateAlternateColorCodes(char altColorChar, String textToTranslate)
     {
         char[] b = textToTranslate.toCharArray();
-        for (int i = 0; i < b.length - 1; i++) 
+        for (int i = 0; i < b.length - 1; i++)
         {
-            if (b[i] == altColorChar && "0123456789AaBbCcDdEeFfKkLlMmNnOoRr".indexOf(b[i+1]) > -1) 
+            if (b[i] == altColorChar && "0123456789AaBbCcDdEeFfKkLlMmNnOoRr".indexOf(b[i + 1]) > -1)
             {
                 b[i] = ChatColor.COLOR_CHAR;
-                b[i+1] = Character.toLowerCase(b[i+1]);
+                b[i + 1] = Character.toLowerCase(b[i + 1]);
             }
         }
         return new String(b);
