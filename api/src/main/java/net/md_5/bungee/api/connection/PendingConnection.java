febb661eb33e5b1b930f86362fa7a68a6271f65b
md_5 <md_5@bigpond.com>
2013-01-12 12:36:41 +1100

    Add more methods to sum up pending connection.

--- api/src/main/java/net/md_5/bungee/api/connection/PendingConnection.java
+++ api/src/main/java/net/md_5/bungee/api/connection/PendingConnection.java
@@ -1,8 +1,31 @@
 package net.md_5.bungee.api.connection;
 
+import java.net.InetSocketAddress;
+
 /**
  * Represents a user attempting to log into the proxy.
  */
 public interface PendingConnection extends Connection
 {
+
+    /**
+     * Get the requested username.
+     *
+     * @return the requested username, or null if not set
+     */
+    public String getName();
+
+    /**
+     * Get the numerical client version of the player attempting to log in.
+     *
+     * @return the protocol version of the remote client
+     */
+    public byte getVersion();
+
+    /**
+     * Get the requested virtual host that the client tried to connect to.
+     *
+     * @return request virtual host or null if invalid / not specified.
+     */
+    public InetSocketAddress getVirtualHost();
 }
