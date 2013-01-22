ccf44336ce81f7ce71fa0a6d242825d973047265
md_5 <md_5@live.com.au>
2013-01-22 15:18:11 +1100

    Fix issue #112, loading plugins

--- api/src/main/java/net/md_5/bungee/api/plugin/PluginDescription.java
+++ api/src/main/java/net/md_5/bungee/api/plugin/PluginDescription.java
@@ -1,28 +1,34 @@
 package net.md_5.bungee.api.plugin;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 /**
  * POJO representing the plugin.yml file.
  */
 @Data
+@AllArgsConstructor
 public class PluginDescription
 {
 
     /**
      * Friendly name of the plugin.
      */
-    private final String name;
+    private String name;
     /**
      * Plugin main class. Needs to extend {@link Plugin}.
      */
-    private final String main;
+    private String main;
     /**
      * Plugin version.
      */
-    private final String version;
+    private String version;
     /**
      * Plugin author.
      */
-    private final String author;
+    private String author;
+
+    public PluginDescription()
+    {
+    }
 }
