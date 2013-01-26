7444fca999b7edb85f2c3ec3e548ac89daa2d2bb
md_5 <md_5@live.com.au>
2013-01-26 14:56:04 +1100

    Make the target of targeted events final.

--- api/src/main/java/net/md_5/bungee/api/event/TargetedEvent.java
+++ api/src/main/java/net/md_5/bungee/api/event/TargetedEvent.java
@@ -7,10 +7,7 @@ import net.md_5.bungee.api.connection.Connection;
 import net.md_5.bungee.api.plugin.Event;
 
 /**
- * An event which occurs in the communication between two nodes. It is not
- * recommended to call {@link #setSender(net.md_5.bungee.api.Connection)} or
- * {@link #setReceiver(net.md_5.bungee.api.Connection)} and the results of doing
- * so are undefined.
+ * An event which occurs in the communication between two nodes.
  */
 @Data
 @AllArgsConstructor
@@ -21,9 +18,9 @@ public abstract class TargetedEvent extends Event
     /**
      * Creator of the action.
      */
-    private Connection sender;
+    private final Connection sender;
     /**
      * Receiver of the action.
      */
-    private Connection receiver;
+    private final Connection receiver;
 }
