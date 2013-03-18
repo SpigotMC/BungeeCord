a12debf6d0e7644d50aeb290037f903c0b838c13
md_5 <md_5@live.com.au>
2013-03-18 17:36:57 +1100

    Revert "Add info on registered channels to /bungee to enable users to help identify cause of random timeouts & resource leaks"
    This reverts commit b3c34815b0fc1ce6ff5803608c19cb4a4eb270df.

--- proxy/src/main/java/net/md_5/bungee/command/CommandBungee.java
+++ proxy/src/main/java/net/md_5/bungee/command/CommandBungee.java
@@ -4,7 +4,6 @@ import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Command;
-import net.md_5.bungee.netty.HandlerBoss;
 
 public class CommandBungee extends Command
 {
@@ -18,6 +17,5 @@ public class CommandBungee extends Command
     public void execute(CommandSender sender, String[] args)
     {
         sender.sendMessage( ChatColor.BLUE + "This server is running BungeeCord version " + ProxyServer.getInstance().getVersion() + " by md_5" );
-        sender.sendMessage( ChatColor.BLUE + "There are currently " + HandlerBoss.registeredChannels + " channels registered" );
     }
 }
