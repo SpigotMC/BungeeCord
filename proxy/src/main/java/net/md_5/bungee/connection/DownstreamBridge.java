f935f93d98ffc6a54feba2a215266da10b7e1232
Robin Lambertz <robinlambertz.dev@gmail.com>
2013-04-09 01:18:00 +0100

    Fix two small typos.

--- proxy/src/main/java/net/md_5/bungee/connection/DownstreamBridge.java
+++ proxy/src/main/java/net/md_5/bungee/connection/DownstreamBridge.java
@@ -153,10 +153,10 @@ public class DownstreamBridge extends PacketHandler
         if ( team.mode == 0 )
         {
             t = new Team( team.name );
-            con.serverSentScoreboard.addTeam( team );
+            con.serverSentScoreboard.addTeam( t );
         } else
         {
-            con.serverSentScoreboard.getTeam( team.name );
+            t = con.serverSentScoreboard.getTeam( team.name );
         }
         
         if ( t != null )
