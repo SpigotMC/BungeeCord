6e9240d5ebf5d02f2f8414d158eab3c3330fdd67
md_5 <md_5@live.com.au>
2013-04-27 22:24:57 +1000

    @normanmaurer, @trustin in relation to netty/1317 can we consider this a motion to allow channel.write(Object,null), I think it is a very good idea and cannot see it breaking any existing functionality at all.

--- proxy/src/main/java/net/md_5/bungee/netty/ChannelWrapper.java
+++ proxy/src/main/java/net/md_5/bungee/netty/ChannelWrapper.java
@@ -1,17 +1,22 @@
 package net.md_5.bungee.netty;
 
 import io.netty.channel.Channel;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 public class ChannelWrapper
 {
 
     private final Channel ch;
+    private final SillyPromise promise;
+
+    public ChannelWrapper(Channel ch)
+    {
+        this.ch = ch;
+        this.promise = new SillyPromise( ch );
+    }
 
     public void write(Object packet)
     {
-        ch.write( packet );
+        ch.write( packet, promise );
     }
 
     public Channel getHandle()
