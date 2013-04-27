ce7c095243b9f93e6f9e95f5046c74d866a550b7
md_5 <md_5@live.com.au>
2013-04-28 08:53:14 +1000

    Make ReusableChannelPromise less hacky

--- proxy/src/main/java/net/md_5/bungee/netty/ChannelWrapper.java
+++ proxy/src/main/java/net/md_5/bungee/netty/ChannelWrapper.java
@@ -6,12 +6,12 @@ public class ChannelWrapper
 {
 
     private final Channel ch;
-    private final SillyPromise promise;
+    private final ReusableChannelPromise promise;
 
     public ChannelWrapper(Channel ch)
     {
         this.ch = ch;
-        this.promise = new SillyPromise( ch );
+        this.promise = new ReusableChannelPromise( ch );
     }
 
     public void write(Object packet)
