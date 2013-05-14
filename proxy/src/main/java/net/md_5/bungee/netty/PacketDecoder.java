0f767550cec7da07464addf705eec31e08a0429d
md_5 <md_5@live.com.au>
2013-05-14 18:32:17 +1000

    Update to latest netty, fix event bus bug, comment and rework PacketDecoder to new netty for better performance

--- proxy/src/main/java/net/md_5/bungee/netty/PacketDecoder.java
+++ proxy/src/main/java/net/md_5/bungee/netty/PacketDecoder.java
@@ -1,6 +1,7 @@
 package net.md_5.bungee.netty;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.MessageBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ReplayingDecoder;
 import lombok.AllArgsConstructor;
@@ -25,13 +26,29 @@ public class PacketDecoder extends ReplayingDecoder<Void>
     private int protocol;
 
     @Override
-    protected byte[] decode(ChannelHandlerContext ctx, ByteBuf in) throws Exception
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, MessageBuf<Object> out) throws Exception
     {
-        int startIndex = in.readerIndex();
-        PacketReader.readPacket( in, protocol );
-        byte[] buf = new byte[ in.readerIndex() - startIndex ];
-        in.readerIndex( startIndex );
-        in.readBytes( buf, 0, buf.length );
-        return buf;
+        // While we have enough data
+        while ( true )
+        {
+            // Store our start index
+            int startIndex = in.readerIndex();
+            // Run packet through framer
+            PacketReader.readPacket( in, protocol );
+            // If we got this far, it means we have formed a packet, so lets grab the end index
+            int endIndex = in.readerIndex();
+            // Allocate a buffer big enough for all bytes we have read
+            byte[] buf = new byte[ endIndex - startIndex ];
+            // Go back to start index
+            in.readerIndex( startIndex );
+            // Drain all the bytes into our buffer
+            in.readBytes( buf, 0, buf.length );
+            // Jump back to the end of this packet
+            in.readerIndex( endIndex );
+            // Checkpoint our state incase we don't have enough data for another packet
+            checkpoint();
+            // Store our decoded message
+            out.add( buf );
+        }
     }
 }
