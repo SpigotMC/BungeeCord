9be44d51a653821883bc57ac7ac7a730fd719eb0
md_5 <md_5@live.com.au>
2013-05-21 11:30:05 +1000

    Update to netty CR3

--- proxy/src/main/java/net/md_5/bungee/netty/ChannelWrapper.java
+++ proxy/src/main/java/net/md_5/bungee/netty/ChannelWrapper.java
@@ -6,17 +6,15 @@ public class ChannelWrapper
 {
 
     private final Channel ch;
-    private final ReusableChannelPromise promise;
 
     public ChannelWrapper(Channel ch)
     {
         this.ch = ch;
-        this.promise = new ReusableChannelPromise( ch );
     }
 
     public void write(Object packet)
     {
-        ch.write( packet, promise );
+        ch.write( packet, ch.voidPromise() );
     }
 
     public Channel getHandle()
