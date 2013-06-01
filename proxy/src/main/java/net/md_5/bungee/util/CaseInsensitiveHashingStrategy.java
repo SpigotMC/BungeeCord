fdc840072709aa97927b62b4549fb20a4f80b886
md_5 <md_5@live.com.au>
2013-06-01 16:07:17 +1000

    Fix comparisons of objects in case insensitive hashmap

--- proxy/src/main/java/net/md_5/bungee/util/CaseInsensitiveHashingStrategy.java
+++ proxy/src/main/java/net/md_5/bungee/util/CaseInsensitiveHashingStrategy.java
@@ -16,6 +16,6 @@ class CaseInsensitiveHashingStrategy implements HashingStrategy
     @Override
     public boolean equals(Object o1, Object o2)
     {
-        return o1 instanceof String && o2 instanceof String && ( (String) o1 ).toLowerCase().equals( ( (String) o2 ).toLowerCase() );
+        return o1.equals( o2 ) || ( o1 instanceof String && o2 instanceof String && ( (String) o1 ).toLowerCase().equals( ( (String) o2 ).toLowerCase() ) );
     }
 }
