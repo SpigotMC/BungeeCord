9cd26ca4100667d10590b8dc0aa1e5cd54eaa61d
md_5 <md_5@live.com.au>
2013-06-10 14:55:57 +1000

    I feel like the time for change is here

--- proxy/src/main/java/net/md_5/bungee/BungeeCord.java
+++ proxy/src/main/java/net/md_5/bungee/BungeeCord.java
@@ -148,7 +148,7 @@ public class BungeeCord extends ProxyServer
     public static void main(String[] args) throws Exception
     {
         Calendar deadline = Calendar.getInstance();
-        deadline.set( 2013, 6, 14 ); // year, month, date
+        deadline.set( 2013, 7, 1 ); // year, month, date
         if ( Calendar.getInstance().after( deadline ) )
         {
             System.err.println( "*** Warning, this build is outdated ***" );
