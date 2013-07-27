deed3c7898410a6aecb2d234c109f0f335c754b0
md_5 <md_5@live.com.au>
2013-07-27 12:12:12 +1000

    Put colours in default motd to try and force quoting in the dumped yaml

--- proxy/src/main/java/net/md_5/bungee/config/YamlConfig.java
+++ proxy/src/main/java/net/md_5/bungee/config/YamlConfig.java
@@ -170,7 +170,7 @@ public class YamlConfig implements ConfigurationAdapter
             Map<String, Object> val = entry.getValue();
             String name = entry.getKey();
             String addr = get( "address", "localhost:25565", val );
-            String motd = ChatColor.translateAlternateColorCodes( '&', get( "motd", "Just another BungeeCord - Forced Host", val ) );
+            String motd = ChatColor.translateAlternateColorCodes( '&', get( "motd", "&1Just another BungeeCord - Forced Host", val ) );
             boolean restricted = get( "restricted", false, val );
             InetSocketAddress address = Util.getAddr( addr );
             ServerInfo info = ProxyServer.getInstance().constructServerInfo( name, address, motd, restricted );
@@ -195,7 +195,7 @@ public class YamlConfig implements ConfigurationAdapter
 
         for ( Map<String, Object> val : base )
         {
-            String motd = get( "motd", "Another Bungee server", val );
+            String motd = get( "motd", "&1Another Bungee server", val );
             motd = ChatColor.translateAlternateColorCodes( '&', motd );
 
             int maxPlayers = get( "max_players", 1, val );
