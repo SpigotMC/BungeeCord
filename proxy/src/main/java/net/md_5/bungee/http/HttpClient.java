f2d122bc327b6fbd84c88c98971a5b6ad2105712
md_5 <md_5@live.com.au>
2013-08-10 07:30:41 +1000

    Fix http client

--- proxy/src/main/java/net/md_5/bungee/http/HttpClient.java
+++ proxy/src/main/java/net/md_5/bungee/http/HttpClient.java
@@ -63,7 +63,7 @@ public class HttpClient
                     HttpRequest request = new DefaultHttpRequest( HttpVersion.HTTP_1_1, HttpMethod.GET, path );
                     request.headers().set( HttpHeaders.Names.HOST, uri.getHost() );
 
-                    future.channel().write( request );
+                    future.channel().writeAndFlush( request );
                 } else
                 {
                     callback.done( null, future.cause() );
