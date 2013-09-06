bb47aba6822ca0cabe1bfc4fc5b88755d7129d8d
Dabo Ross <dabodialga@gmail.com>
2013-09-05 21:59:48 -0700

    Add UnregisteringListenerTest

---
+++ event/src/test/java/net/md_5/bungee/event/UnregisteringListenerTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2013 Dabo Ross <www.daboross.net>
+ */
+package net.md_5.bungee.event;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author daboross
+ */
+public class UnregisteringListenerTest
+{
+
+    private final EventBus bus = new EventBus();
+
+    @Test
+    public void testPriority()
+    {
+        bus.register( this );
+        bus.unregister( this );
+        bus.post( new TestEvent() );
+    }
+
+    @EventHandler
+    public void onEvent(TestEvent evt)
+    {
+        Assert.fail( "Event listener wasn't unregistered" );
+    }
+
+    public static class TestEvent
+    {
+    }
+}
