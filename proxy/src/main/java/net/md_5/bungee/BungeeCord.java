e8377620827cd1234141e70ce1724b5978fcd6f2
md_5 <md_5@live.com.au>
2013-09-08 11:49:27 +1000

    Bump date

--- proxy/src/main/java/net/md_5/bungee/BungeeCord.java
+++ proxy/src/main/java/net/md_5/bungee/BungeeCord.java
@@ -168,7 +168,7 @@ public class BungeeCord extends ProxyServer
     public static void main(String[] args) throws Exception
     {
         Calendar deadline = Calendar.getInstance();
-        deadline.set( 2013, 9, 10 ); // year, month, date
+        deadline.set( 2013, 9, 31 ); // year, month, date
         if ( Calendar.getInstance().after( deadline ) )
         {
             System.err.println( "*** Warning, this build is outdated ***" );
