3e8c21a4854295e43e8252fadc70efa003c50ab6
md_5 <md_5@live.com.au>
2013-09-21 16:57:17 +1000

    Add specific exception for bad packets.

---
+++ protocol/src/main/java/net/md_5/bungee/protocol/BadPacketException.java
@@ -0,0 +1,10 @@
+package net.md_5.bungee.protocol;
+
+public class BadPacketException extends RuntimeException
+{
+
+    public BadPacketException(String message)
+    {
+        super( message );
+    }
+}
