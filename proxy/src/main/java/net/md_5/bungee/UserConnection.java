2ab926c1560e91ceebd629c157fc6597134f60e7
md_5 <md_5@live.com.au>
2013-10-27 18:09:27 +1100

    Fix kicks?

--- proxy/src/main/java/net/md_5/bungee/UserConnection.java
+++ proxy/src/main/java/net/md_5/bungee/UserConnection.java
@@ -255,7 +255,7 @@ public final class UserConnection implements ProxiedPlayer
         if ( ch.getHandle().isActive() )
         {
             bungee.getLogger().log( Level.INFO, "[" + getName() + "] disconnected with: " + reason );
-            unsafe().sendPacket( new Kick( Util.stupify( reason ) ) );
+            unsafe().sendPacket( new Kick( reason ) );
             ch.close();
             if ( server != null )
             {
