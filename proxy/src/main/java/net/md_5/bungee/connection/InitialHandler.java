4040d9f20a5fbc5fac2147379a334496818ed37f
md_5 <md_5@live.com.au>
2013-11-01 22:00:46 +1100

    [Breaking] Fix player online sample

--- proxy/src/main/java/net/md_5/bungee/connection/InitialHandler.java
+++ proxy/src/main/java/net/md_5/bungee/connection/InitialHandler.java
@@ -123,7 +123,7 @@ public class InitialHandler extends PacketHandler implements PendingConnection
     public void handle(LegacyPing ping) throws Exception
     {
         ServerPing legacy = new ServerPing( new ServerPing.Protocol( bungee.getGameVersion(), bungee.getProtocolVersion() ),
-                new ServerPing.Players( listener.getMaxPlayers(), bungee.getOnlineCount() ), null, listener.getMotd(), null );
+                new ServerPing.Players( listener.getMaxPlayers(), bungee.getOnlineCount(), null ), listener.getMotd(), null );
         legacy = bungee.getPluginManager().callEvent( new ProxyPingEvent( this, legacy ) ).getResponse();
 
         String kickMessage = ChatColor.DARK_BLUE
@@ -169,8 +169,8 @@ public class InitialHandler extends PacketHandler implements PendingConnection
         {
             pingBack.done( new ServerPing(
                     new ServerPing.Protocol( bungee.getGameVersion(), bungee.getProtocolVersion() ),
-                    new ServerPing.Players( listener.getMaxPlayers(), bungee.getOnlineCount() ),
-                    null, motd, BungeeCord.getInstance().config.favicon ),
+                    new ServerPing.Players( listener.getMaxPlayers(), bungee.getOnlineCount(), null ),
+                    motd, BungeeCord.getInstance().config.favicon ),
                     null );
         }
 
