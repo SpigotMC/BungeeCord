c87fc31084314a73d987255e09cbb2424b579697
Moehritz <moehritz@gmx.de>
2013-11-10 16:17:43 +0100

    Add some comments

--- api/src/main/java/net/md_5/bungee/api/Location.java
+++ api/src/main/java/net/md_5/bungee/api/Location.java
@@ -1,35 +1,79 @@
 package net.md_5.bungee.api;
 
-import lombok.ToString;
 import net.md_5.bungee.api.connection.Server;
 
 public interface Location
 {
 
+    /**
+     * Gets the x-value of the location
+     */
     double getX();
 
+    /**
+     * Gets the y-value of the location
+     */
     double getY();
 
+    /**
+     * Gets the z-value of the location
+     */
     double getZ();
 
+    /**
+     * Gets the yaw-value of the location
+     */
     float getYaw();
 
+    /**
+     * Gets the pitch-value of the location
+     */
     float getPitch();
 
+    /**
+     * Gets the server this player is connected to
+     */
     Server getServer();
 
+    /**
+     * Gets the current dimension of this player
+     */
     int getDimension();
 
+    /**
+     * Sets a new x for this location This wont affect the players real
+     * position!
+     */
     void setX(double x);
 
+    /**
+     * Sets a new y for this location This wont affect the players real
+     * position!
+     */
     void setY(double y);
 
+    /**
+     * Sets a new z for this location This wont affect the players real
+     * position!
+     */
     void setZ(double z);
 
+    /**
+     * Sets a new yaw for this location This wont affect the players real
+     * position!
+     */
     void setYaw(float yaw);
 
+    /**
+     * Sets a new pitch for this location This wont affect the players real
+     * position!
+     */
     void setPitch(float pitch);
 
+    /**
+     * Sets a new dimension for this location This wont affect the players real
+     * position!
+     */
     void setDimension(int dimension);
 
     public class Dimension
