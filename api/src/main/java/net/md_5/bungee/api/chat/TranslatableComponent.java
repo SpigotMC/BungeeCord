31be1f33dfbd79250dca32fb62cf8303a8fdce1b
Thinkofdeath <thethinkofdeath@gmail.com>
2013-12-06 23:18:10 +0000

    Support printing TranslatableComponents

--- api/src/main/java/net/md_5/bungee/api/chat/TranslatableComponent.java
+++ api/src/main/java/net/md_5/bungee/api/chat/TranslatableComponent.java
@@ -3,15 +3,18 @@ package net.md_5.bungee.api.chat;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import net.md_5.bungee.api.ChatColor;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class TranslatableComponent extends BaseComponent
 {
+    public final ResourceBundle locales = ResourceBundle.getBundle( "en_US" );
 
     private String translate;
     private List<BaseComponent> with;
@@ -44,14 +47,44 @@ public class TranslatableComponent extends BaseComponent
     @Override
     protected void toPlainText(StringBuilder builder)
     {
-        //TODO
+        String[] parts = translate.split( "((?<=%s)|(?=%s))" );
+        int i = 0;
+        for ( String part : parts )
+        {
+            if ( part.equals( "%s" ) )
+            {
+                with.get( i ).toPlainText( builder );
+                i++;
+            } else
+            {
+                builder.append( part );
+            }
+        }
         super.toPlainText( builder );
     }
 
     @Override
     protected void toLegacyText(StringBuilder builder)
     {
-        //TODO
+        String[] parts = locales.getString( translate ).split( "((?<=%s)|(?=%s))" );
+        int i = 0;
+        for ( String part : parts )
+        {
+            if ( part.equals( "%s" ) )
+            {
+                with.get( i ).toLegacyText( builder );
+                i++;
+            } else
+            {
+                builder.append( getColor() );
+                if ( isBold() ) builder.append( ChatColor.BOLD );
+                if ( isItalic() ) builder.append( ChatColor.ITALIC );
+                if ( isUnderlined() ) builder.append( ChatColor.UNDERLINE );
+                if ( isStrikethrough() ) builder.append( ChatColor.STRIKETHROUGH );
+                if ( isObfuscated() ) builder.append( ChatColor.MAGIC );
+                builder.append( part );
+            }
+        }
         super.toLegacyText( builder );
     }
 
