8bad5354ab1808024c768bb20aac82ee3365fa80
kosgan10 <kosgan10@gmail.com>
2013-12-30 15:38:13 +0200

    Fix imports

--- proxy/src/main/java/net/md_5/bungee/command/CommandAlertRaw.java
+++ proxy/src/main/java/net/md_5/bungee/command/CommandAlertRaw.java
@@ -1,15 +1,17 @@
 package net.md_5.bungee.command;
 
+import com.google.common.base.Joiner;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.chat.ComponentSerializer;
 
-import com.google.common.base.Joiner;
+import java.util.Arrays;
 
 public class CommandAlertRaw extends Command
 {
