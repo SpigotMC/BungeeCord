4be58a7c009fd4d1692c5ffdef40deb56be44c1a
md_5 <git@md-5.net>
2014-01-10 09:53:33 +1100

    Copy across all the commands and add their bootstrap plugins.

---
+++ module/cmd-alert/src/main/java/net/md_5/bungee/module/cmd/alert/PluginAlert.java
@@ -0,0 +1,12 @@
+package net.md_5.bungee.module.cmd.alert;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class PluginAlert extends Plugin {
+
+    @Override
+    public void onEnable() {
+        getProxy().getPluginManager().registerCommand(this, new CommandAlert());
+        getProxy().getPluginManager().registerCommand(this, new CommandAlertRaw());
+    }
+}
