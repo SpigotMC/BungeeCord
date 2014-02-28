6cd586fe28b8c56a367eb323e129520c75468e13
kamcio96 <kamcio96@users.noreply.github.com>
2014-02-28 22:44:48 +0100

    Add ProxyReloadEvent

--- proxy/src/main/java/net/md_5/bungee/command/CommandReload.java
+++ proxy/src/main/java/net/md_5/bungee/command/CommandReload.java
@@ -4,6 +4,7 @@ import net.md_5.bungee.BungeeCord;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.api.event.ProxyReloadEvent;
 
 public class CommandReload extends Command
 {
@@ -19,6 +20,8 @@ public class CommandReload extends Command
         BungeeCord.getInstance().config.load();
         BungeeCord.getInstance().stopListeners();
         BungeeCord.getInstance().startListeners();
+        BungeeCord.getInstance().getPluginManager().callEvent(new ProxyReloadEvent(sender));
+
         sender.sendMessage( ChatColor.BOLD.toString() + ChatColor.RED.toString() + "BungeeCord has been reloaded."
                 + " This is NOT advisable and you will not be supported with any issues that arise! Please restart BungeeCord ASAP." );
     }
