b6fb1c0b061ce85b060d82c5d4b0000e251cee48
Thinkofdeath <thethinkofdeath@gmail.com>
2014-04-09 21:01:33 +0100

    Remove @RequiredArgsConstructor from PlayerInfoSerializer as the javadoc fails to build with it

--- proxy/src/main/java/net/md_5/bungee/PlayerInfoSerializer.java
+++ proxy/src/main/java/net/md_5/bungee/PlayerInfoSerializer.java
@@ -7,17 +7,20 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
-import lombok.RequiredArgsConstructor;
 import net.md_5.bungee.api.ServerPing;
 
 import java.lang.reflect.Type;
 import java.util.UUID;
 
-@RequiredArgsConstructor
 public class PlayerInfoSerializer implements JsonSerializer<ServerPing.PlayerInfo>, JsonDeserializer<ServerPing.PlayerInfo>
 {
 
     private final int protocol;
+
+    public PlayerInfoSerializer(int protocol)
+    {
+        this.protocol = protocol;
+    }
 
     @Override
     public ServerPing.PlayerInfo deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException
