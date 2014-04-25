9e46739343632039658f0449f58074d1856feaea
Thinkofdeath <thethinkofdeath@gmail.com>
2014-04-25 20:53:10 +0100

    Improve the CommandServer tooltip
    Includes the grammar fix from #953

--- module/cmd-server/src/main/java/net/md_5/bungee/module/cmd/server/CommandServer.java
+++ module/cmd-server/src/main/java/net/md_5/bungee/module/cmd/server/CommandServer.java
@@ -48,7 +48,11 @@ public class CommandServer extends Command implements TabExecutor
                 if ( server.canAccess( player ) )
                 {
                     TextComponent serverTextComponent = new TextComponent( first ? server.getName() : ", " + server.getName() );
-                    serverTextComponent.setHoverEvent( new HoverEvent( HoverEvent.Action.SHOW_TEXT, new ComponentBuilder( server.getPlayers().size() + " players" ).create() ) );
+                    int count = server.getPlayers().size();
+                    serverTextComponent.setHoverEvent( new HoverEvent( HoverEvent.Action.SHOW_TEXT,
+                            new ComponentBuilder( count + (count == 1 ? " player" : " players") + "\n")
+                                    .append( "Click to connect to the server" ).italic( true )
+                                    .create() ) );
                     serverTextComponent.setClickEvent( new ClickEvent( ClickEvent.Action.RUN_COMMAND, "/server " + server.getName() ) );
                     serverList.addExtra( serverTextComponent );
                     first = false;
