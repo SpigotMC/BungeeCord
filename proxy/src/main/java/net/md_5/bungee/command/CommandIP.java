59ba644623d22dd9837ac1a018bf1ab0e4e540a5
md_5 <git@md-5.net>
2014-06-23 17:16:14 +1000

    Make IP command tabbable - closes #999

--- proxy/src/main/java/net/md_5/bungee/command/CommandIP.java
+++ proxy/src/main/java/net/md_5/bungee/command/CommandIP.java
@@ -4,9 +4,8 @@ import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
-import net.md_5.bungee.api.plugin.Command;
 
-public class CommandIP extends Command
+public class CommandIP extends PlayerCommand
 {
 
     public CommandIP()
