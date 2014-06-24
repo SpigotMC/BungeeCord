20902afea1782310ea510e76a843f63b680ba83b
md_5 <git@md-5.net>
2014-06-24 18:21:13 +1000

    Calling parent logger seems to work nowadays - closes #1071

--- api/src/main/java/net/md_5/bungee/api/plugin/PluginLogger.java
+++ api/src/main/java/net/md_5/bungee/api/plugin/PluginLogger.java
@@ -2,23 +2,23 @@ package net.md_5.bungee.api.plugin;
 
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
-import net.md_5.bungee.api.ProxyServer;
 
 public class PluginLogger extends Logger
 {
 
-    private String pluginName;
+    private final String pluginName;
 
     protected PluginLogger(Plugin plugin)
     {
         super( plugin.getClass().getCanonicalName(), null );
         pluginName = "[" + plugin.getDescription().getName() + "] ";
+        setParent( plugin.getProxy().getLogger() );
     }
 
     @Override
     public void log(LogRecord logRecord)
     {
         logRecord.setMessage( pluginName + logRecord.getMessage() );
-        ProxyServer.getInstance().getLogger().log( logRecord );
+        super.log( logRecord );
     }
 }
