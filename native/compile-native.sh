e249a37fcd37386dcb66f45f69559a39338584bb
md_5 <git@md-5.net>
2014-07-01 20:13:30 +1000

    Update native cipher to make use of the OpenSSL EVP API so that it can actually utilise hardware acceleration and other goodies.
    Whereas before OpenSSL would often lose benchmarks now it always wins.

--- native/compile-native.sh
+++ native/compile-native.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gcc -shared -fPIC -O3 -Werror -I$JAVA_HOME/include/ -I$JAVA_HOME/include/linux/ src/main/c/NativeCipherImpl.c -o src/main/resources/native-cipher.so -lcrypto
+g++ -shared -fPIC -O3 -Werror -I$JAVA_HOME/include/ -I$JAVA_HOME/include/linux/ src/main/c/NativeCipherImpl.cpp -o src/main/resources/native-cipher.so -lcrypto
---
+++ native/src/main/c/NativeCipherImpl.cpp
@@ -0,0 +1,22 @@
+#include <openssl/evp.h>
+#include "net_md_5_bungee_NativeCipherImpl.h"
+
+typedef unsigned char byte;
+
+jlong JNICALL Java_net_md_15_bungee_NativeCipherImpl_init(JNIEnv* env, jobject obj, jboolean forEncryption, jbyteArray key) {
+    jbyte *keyBytes = env->GetByteArrayElements(key, NULL);
+
+    EVP_CIPHER_CTX *cipherCtx = new EVP_CIPHER_CTX();
+    EVP_CipherInit(cipherCtx, EVP_aes_128_cfb8(), (byte*) keyBytes, (byte*) keyBytes, forEncryption);
+
+    env->ReleaseByteArrayElements(key, keyBytes, JNI_ABORT);
+    return (jlong) cipherCtx;
+}
+
+void Java_net_md_15_bungee_NativeCipherImpl_free(JNIEnv* env, jobject obj, jlong ctx) {
+    EVP_CIPHER_CTX_free((EVP_CIPHER_CTX*) ctx);
+}
+
+void Java_net_md_15_bungee_NativeCipherImpl_cipher(JNIEnv* env, jobject obj, jlong ctx, jlong in, jlong out, jint length) {
+    EVP_CipherUpdate((EVP_CIPHER_CTX*) ctx, (byte*) out, &length, (byte*) in, length);
+}
