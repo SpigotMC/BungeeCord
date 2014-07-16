be13a003867334330a31272518fd0bca624cd936
md_5 <git@md-5.net>
2014-07-16 17:12:52 +1000

    Add missing return - closes #1114

--- proxy/src/main/java/net/md_5/bungee/connection/InitialHandler.java
+++ proxy/src/main/java/net/md_5/bungee/connection/InitialHandler.java
@@ -363,6 +363,7 @@ public class InitialHandler extends PacketHandler implements PendingConnection
                 if ( result.isCancelled() )
                 {
                     disconnect( result.getCancelReason() );
+                    return;
                 }
                 if ( ch.isClosed() )
                 {
