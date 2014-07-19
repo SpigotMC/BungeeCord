77a601385d9e0cf6764e19c223e01c4b92570362
md_5 <git@md-5.net>
2014-07-19 21:22:20 +1000

    Don't let Bungee run in dirs with ! in name.
    Java uses ! to indicate a resource inside of a jar/zip/other container. Running Bungee from within a directory that has a ! will cause this to muck up.

--- proxy/src/main/java/net/md_5/bungee/BungeeCord.java
+++ proxy/src/main/java/net/md_5/bungee/BungeeCord.java
@@ -162,6 +162,9 @@ public class BungeeCord extends ProxyServer
     @SuppressFBWarnings("DM_DEFAULT_ENCODING")
     public BungeeCord() throws IOException
     {
+        // Java uses ! to indicate a resource inside of a jar/zip/other container. Running Bungee from within a directory that has a ! will cause this to muck up.
+        Preconditions.checkState( new File( "." ).getAbsolutePath().indexOf( '!' ) == -1, "Cannot use BungeeCord in directory with ! in path." );
+
         System.setSecurityManager( new BungeeSecurityManager() );
 
         try
