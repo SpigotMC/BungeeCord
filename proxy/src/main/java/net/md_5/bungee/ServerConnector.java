6e44a0c92faf4a3a1c4a3556b91284eda67ae224
Melair <melair@caldonia.net>
2014-07-28 22:10:03 +0100

    Allow customisation of kicking during initial server connection. In particular allow removal of server name by modification of messages.properties.

--- proxy/src/main/java/net/md_5/bungee/ServerConnector.java
+++ proxy/src/main/java/net/md_5/bungee/ServerConnector.java
@@ -217,7 +217,7 @@ public class ServerConnector extends PacketHandler
             throw CancelSendSignal.INSTANCE;
         }
 
-        String message = bungee.getTranslation( "connect_kick" ) + target.getName() + ": " + event.getKickReason();
+        String message = bungee.getTranslation( "connect_kick", target.getName() ) + event.getKickReason();
         if ( user.isDimensionChange() )
         {
             user.disconnect( message );
