d6030ff03bcfeb9007cb20eb81485916b31c2f6e
Minecrell <dev@minecrell.net>
2014-08-25 11:49:56 +0200

    Remove unused import.

--- proxy/src/main/java/net/md_5/bungee/BungeeCord.java
+++ proxy/src/main/java/net/md_5/bungee/BungeeCord.java
@@ -31,7 +31,6 @@ import net.md_5.bungee.conf.Configuration;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
