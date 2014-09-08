2d568ea08bcd7709f767741007c71872479b3da0
Minecrell <dev@minecrell.net>
2014-09-08 21:15:14 +0200

    Add chat position API.

---
+++ chat/src/main/java/net/md_5/bungee/api/ChatMessageType.java
@@ -0,0 +1,11 @@
+package net.md_5.bungee.api;
+
+/**
+ * Represents the position on the screen where a message will appear.
+ */
+public enum ChatMessageType
+{
+    CHAT,
+    SYSTEM,
+    ACTION_BAR
+}
