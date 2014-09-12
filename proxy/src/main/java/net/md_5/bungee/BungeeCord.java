9f526a215ced1af9ef03c0650a1f858acb466eaf
mrapple <tony@oc.tc>
2014-09-11 23:33:52 -0500

    Disable security manager

--- proxy/src/main/java/net/md_5/bungee/BungeeCord.java
+++ proxy/src/main/java/net/md_5/bungee/BungeeCord.java
@@ -171,7 +171,8 @@ public class BungeeCord extends ProxyServer
         // Java uses ! to indicate a resource inside of a jar/zip/other container. Running Bungee from within a directory that has a ! will cause this to muck up.
         Preconditions.checkState( new File( "." ).getAbsolutePath().indexOf( '!' ) == -1, "Cannot use BungeeCord in directory with ! in path." );
 
-        System.setSecurityManager( new BungeeSecurityManager() );
+        // Overcast - disable security manager
+        // System.setSecurityManager( new BungeeSecurityManager() );
 
         try
         {
