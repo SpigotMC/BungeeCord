40f3a58545db2dc2eb25795ad4876be8a3c9f709
Minecrell <dev@minecrell.net>
2014-09-14 18:23:57 +0200

    Add methods to load configurations with defaults.
    Add Configuration constructor for empty configurations.
    
    Use defaults for the Configuration object getter.

--- config/src/main/java/net/md_5/bungee/config/YamlConfiguration.java
+++ config/src/main/java/net/md_5/bungee/config/YamlConfiguration.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.Reader;
 import java.io.Writer;
 import java.util.LinkedHashMap;
@@ -46,33 +47,69 @@ public class YamlConfiguration extends ConfigurationProvider
     @Override
     public Configuration load(File file) throws IOException
     {
+        return load( file, null );
+    }
+
+    @Override
+    public Configuration load(File file, Configuration defaults) throws IOException
+    {
         try ( FileReader reader = new FileReader( file ) )
         {
-            return load( reader );
+            return load( reader, defaults );
         }
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public Configuration load(Reader reader)
+    {
+        return load( reader, null );
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Configuration load(Reader reader, Configuration defaults)
     {
         Map<String, Object> map = yaml.get().loadAs( reader, LinkedHashMap.class );
         if ( map == null )
         {
             map = new LinkedHashMap<>();
         }
-        return new Configuration( map, null );
+        return new Configuration( map, defaults );
+    }
+
+    @Override
+    public Configuration load(InputStream is)
+    {
+        return load( is, null );
     }
 
     @Override
     @SuppressWarnings("unchecked")
+    public Configuration load(InputStream is, Configuration defaults)
+    {
+        Map<String, Object> map = yaml.get().loadAs( is, LinkedHashMap.class );
+        if ( map == null )
+        {
+            map = new LinkedHashMap<>();
+        }
+        return new Configuration( map, defaults );
+    }
+
+    @Override
     public Configuration load(String string)
+    {
+        return load( string, null );
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Configuration load(String string, Configuration defaults)
     {
         Map<String, Object> map = yaml.get().loadAs( string, LinkedHashMap.class );
         if ( map == null )
         {
             map = new LinkedHashMap<>();
         }
-        return new Configuration( map, null );
+        return new Configuration( map, defaults );
     }
 }
