e9966547255735958524f28c4976c3a3e8033cec
kamcio96 <k.nadworski@icloud.com>
2014-09-15 16:17:37 +0200

    Dont check direction in encode method. Performance

--- protocol/src/main/java/net/md_5/bungee/protocol/MinecraftEncoder.java
+++ protocol/src/main/java/net/md_5/bungee/protocol/MinecraftEncoder.java
@@ -3,15 +3,18 @@ package net.md_5.bungee.protocol;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
-import lombok.AllArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
 public class MinecraftEncoder extends MessageToByteEncoder<DefinedPacket>
 {
 
-    @Setter
-    private Protocol protocol;
+    public MinecraftEncoder( Protocol protocol, boolean server, int protocolVersion ){
+        this.server = server;
+        this.protocolVersion = protocolVersion;
+        setProtocol(protocol);
+    }
+
+    private Protocol.DirectionData prot;
     private boolean server;
     @Setter
     private int protocolVersion;
@@ -19,8 +22,15 @@ public class MinecraftEncoder extends MessageToByteEncoder<DefinedPacket>
     @Override
     protected void encode(ChannelHandlerContext ctx, DefinedPacket msg, ByteBuf out) throws Exception
     {
-        Protocol.DirectionData prot = ( server ) ? protocol.TO_CLIENT : protocol.TO_SERVER;
         DefinedPacket.writeVarInt( prot.getId( msg.getClass() ), out );
         msg.write( out, prot.getDirection(), protocolVersion );
+    }
+
+    public void setProtocol(Protocol protocol){
+        if(server){
+            prot = protocol.TO_CLIENT;
+        } else {
+            prot = protocol.TO_SERVER;
+        }
     }
 }
