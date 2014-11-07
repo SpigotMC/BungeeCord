56c372a3ce64386de9a80df6b5992a0ad57e22d2
md_5 <git@md-5.net>
2014-11-08 10:02:48 +1100

    Fix HTTP client now that Mojang has their stuff together.

--- proxy/src/main/java/net/md_5/bungee/http/HttpInitializer.java
+++ proxy/src/main/java/net/md_5/bungee/http/HttpInitializer.java
@@ -3,12 +3,11 @@ package net.md_5.bungee.http;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
 import io.netty.handler.codec.http.HttpClientCodec;
+import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslHandler;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import java.util.concurrent.TimeUnit;
-import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLEngine;
-import javax.net.ssl.TrustManager;
 import lombok.RequiredArgsConstructor;
 import net.md_5.bungee.api.Callback;
 
@@ -18,6 +17,8 @@ public class HttpInitializer extends ChannelInitializer<Channel>
 
     private final Callback<String> callback;
     private final boolean ssl;
+    private final String host;
+    private final int port;
 
     @Override
     protected void initChannel(Channel ch) throws Exception
@@ -25,14 +26,7 @@ public class HttpInitializer extends ChannelInitializer<Channel>
         ch.pipeline().addLast( "timeout", new ReadTimeoutHandler( HttpClient.TIMEOUT, TimeUnit.MILLISECONDS ) );
         if ( ssl )
         {
-            SSLContext context = SSLContext.getInstance( "TLS" );
-            context.init( null, new TrustManager[]
-            {
-                TrustingX509Manager.getInstance()
-            }, null );
-
-            SSLEngine engine = context.createSSLEngine();
-            engine.setUseClientMode( true );
+            SSLEngine engine = SslContext.newClientContext().newEngine( ch.alloc(), host, port );
 
             ch.pipeline().addLast( "ssl", new SslHandler( engine ) );
         }
