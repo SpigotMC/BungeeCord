857f1fa3f1970804f16d7f87c391f791ed1ca9fc
Jedediah Smith <jedediah@silencegreys.com>
2014-11-19 04:07:51 -0500

    Log stderr at WARNING instead of SEVERE

--- proxy/src/main/java/net/md_5/bungee/BungeeCord.java
+++ proxy/src/main/java/net/md_5/bungee/BungeeCord.java
@@ -197,7 +197,9 @@ public class BungeeCord extends ProxyServer
         consoleReader.setExpandEvents( false );
 
         logger = new BungeeLogger( this );
-        System.setErr( new PrintStream( new LoggingOutputStream( logger, Level.SEVERE ), true ) );
+
+        // Overcast - stderr gets a lot of non-error output, so log it at WARNING level instead of SEVERE
+        System.setErr( new PrintStream( new LoggingOutputStream( logger, Level.WARNING ), true ) );
         System.setOut( new PrintStream( new LoggingOutputStream( logger, Level.INFO ), true ) );
 
         if ( EncryptionUtil.nativeFactory.load() )
