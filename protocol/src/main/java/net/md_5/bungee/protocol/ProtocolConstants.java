add3a23b3bf274e18624ede40df29578076e5ef7
Thinkofdeath <thinkofdeath@spigotmc.org>
2015-08-17 15:36:27 +0100

    Display all supported versions in the status reply

--- protocol/src/main/java/net/md_5/bungee/protocol/ProtocolConstants.java
+++ protocol/src/main/java/net/md_5/bungee/protocol/ProtocolConstants.java
@@ -1,9 +1,16 @@
 package net.md_5.bungee.protocol;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ProtocolConstants
 {
     public static final int MINECRAFT_1_8 = 47;
     public static final int MINECRAFT_SNAPSHOT = 57;
+    public static final List<String> SUPPORTED_VERSIONS = Arrays.asList(
+            "1.8.X",
+            "15w33c"
+    );
 
     public enum Direction
     {
