b8a28439ea1ed36b9964760fa5f48a27301b9588
Thinkofdeath <thinkofdeath@spigotmc.org>
2015-08-17 15:26:59 +0100

    Update to 15w33c

--- protocol/src/main/java/net/md_5/bungee/protocol/ProtocolConstants.java
+++ protocol/src/main/java/net/md_5/bungee/protocol/ProtocolConstants.java
@@ -3,7 +3,7 @@ package net.md_5.bungee.protocol;
 public class ProtocolConstants
 {
     public static final int MINECRAFT_1_8 = 47;
-    public static final int MINECRAFT_SNAPSHOT = 54;
+    public static final int MINECRAFT_SNAPSHOT = 57;
 
     public enum Direction
     {
