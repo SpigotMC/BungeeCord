263b4f019e51914322587029ab95d4c90f79938e
Aikar <aikar@aikar.co>
2015-09-11 23:50:52 -0400

    Allow configuration of the log file path/filename

--- proxy/src/main/java/net/md_5/bungee/log/BungeeLogger.java
+++ proxy/src/main/java/net/md_5/bungee/log/BungeeLogger.java
@@ -27,7 +27,7 @@ public class BungeeLogger extends Logger
 
         try
         {
-            FileHandler fileHandler = new FileHandler( "proxy.log", 1 << 24, 8, true );
+            FileHandler fileHandler = new FileHandler( System.getProperty("bungee.log-file", "proxy.log"), 1 << 24, 8, true );
             fileHandler.setFormatter( formatter );
             addHandler( fileHandler );
 
