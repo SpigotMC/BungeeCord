b295500e9a769ed1843118b3415ebf6e0f6173b4
md_5 <git@md-5.net>
2015-11-12 10:19:19 +1100

    Update most of the EntityMap to the snapshot.

--- proxy/src/main/java/net/md_5/bungee/entitymap/EntityMap_SNAPSHOT.java
+++ proxy/src/main/java/net/md_5/bungee/entitymap/EntityMap_SNAPSHOT.java
@@ -15,35 +15,34 @@ class EntityMap_SNAPSHOT extends EntityMap
 
     EntityMap_SNAPSHOT()
     {
-        addRewrite( 0x04, ProtocolConstants.Direction.TO_CLIENT, true ); // Entity Equipment
-        addRewrite( 0x0A, ProtocolConstants.Direction.TO_CLIENT, true ); // Use bed
-        addRewrite( 0x0B, ProtocolConstants.Direction.TO_CLIENT, true ); // Animation
-        addRewrite( 0x0C, ProtocolConstants.Direction.TO_CLIENT, true ); // Spawn Player
-        addRewrite( 0x0D, ProtocolConstants.Direction.TO_CLIENT, true ); // Collect Item
-        addRewrite( 0x0E, ProtocolConstants.Direction.TO_CLIENT, true ); // Spawn Object
-        addRewrite( 0x0F, ProtocolConstants.Direction.TO_CLIENT, true ); // Spawn Mob
-        addRewrite( 0x10, ProtocolConstants.Direction.TO_CLIENT, true ); // Spawn Painting
-        addRewrite( 0x11, ProtocolConstants.Direction.TO_CLIENT, true ); // Spawn Experience Orb
-        addRewrite( 0x12, ProtocolConstants.Direction.TO_CLIENT, true ); // Entity Velocity
-        addRewrite( 0x14, ProtocolConstants.Direction.TO_CLIENT, true ); // Entity
-        addRewrite( 0x15, ProtocolConstants.Direction.TO_CLIENT, true ); // Entity Relative Move
-        addRewrite( 0x16, ProtocolConstants.Direction.TO_CLIENT, true ); // Entity Look
-        addRewrite( 0x17, ProtocolConstants.Direction.TO_CLIENT, true ); // Entity Look and Relative Move
-        addRewrite( 0x18, ProtocolConstants.Direction.TO_CLIENT, true ); // Entity Teleport
-        addRewrite( 0x19, ProtocolConstants.Direction.TO_CLIENT, true ); // Entity Head Look
-        addRewrite( 0x1A, ProtocolConstants.Direction.TO_CLIENT, false ); // Entity Status
-        addRewrite( 0x1B, ProtocolConstants.Direction.TO_CLIENT, false ); // Attach Entity
-        addRewrite( 0x1C, ProtocolConstants.Direction.TO_CLIENT, true ); // Entity Metadata
-        addRewrite( 0x1D, ProtocolConstants.Direction.TO_CLIENT, true ); // Entity Effect
-        addRewrite( 0x1E, ProtocolConstants.Direction.TO_CLIENT, true ); // Remove Entity Effect
-        addRewrite( 0x20, ProtocolConstants.Direction.TO_CLIENT, true ); // Entity Properties
-        addRewrite( 0x25, ProtocolConstants.Direction.TO_CLIENT, true ); // Block Break Animation
+        addRewrite( 0x00, ProtocolConstants.Direction.TO_CLIENT, true ); // Spawn Object : PacketPlayOutSpawnEntity
+        addRewrite( 0x01, ProtocolConstants.Direction.TO_CLIENT, true ); // Spawn Experience Orb : PacketPlayOutSpawnEntityExperienceOrb
+        addRewrite( 0x03, ProtocolConstants.Direction.TO_CLIENT, true ); // Spawn Mob : PacketPlayOutSpawnEntityLiving
+        addRewrite( 0x04, ProtocolConstants.Direction.TO_CLIENT, true ); // Spawn Painting : PacketPlayOutSpawnEntityPainting
+        addRewrite( 0x05, ProtocolConstants.Direction.TO_CLIENT, true ); // Spawn Player : PacketPlayOutNamedEntitySpawn
+        addRewrite( 0x06, ProtocolConstants.Direction.TO_CLIENT, true ); // Animation : PacketPlayOutAnimation
+        addRewrite( 0x08, ProtocolConstants.Direction.TO_CLIENT, true ); // Block Break Animation : PacketPlayOutBlockBreakAnimation
+        addRewrite( 0x1B, ProtocolConstants.Direction.TO_CLIENT, false ); // Entity Status : PacketPlayOutEntityStatus
+        addRewrite( 0x26, ProtocolConstants.Direction.TO_CLIENT, true ); // Entity Relative Move : PacketPlayOutRelEntityMove
+        addRewrite( 0x27, ProtocolConstants.Direction.TO_CLIENT, true ); // Entity Look and Relative Move : PacketPlayOutRelEntityMoveLook
+        addRewrite( 0x28, ProtocolConstants.Direction.TO_CLIENT, true ); // Entity Look : PacketPlayOutEntityLook
+        addRewrite( 0x29, ProtocolConstants.Direction.TO_CLIENT, true ); // Entity : PacketPlayOutEntity
         addRewrite( 0x2C, ProtocolConstants.Direction.TO_CLIENT, true ); // Spawn Global Entity
-        addRewrite( 0x43, ProtocolConstants.Direction.TO_CLIENT, true ); // Camera
-        addRewrite( 0x49, ProtocolConstants.Direction.TO_CLIENT, true ); // Update Entity NBT
+        addRewrite( 0x30, ProtocolConstants.Direction.TO_CLIENT, true ); // Use bed : PacketPlayOutBed
+        addRewrite( 0x32, ProtocolConstants.Direction.TO_CLIENT, true ); // Remove Entity Effect : PacketPlayOutRemoveEntityEffect
+        addRewrite( 0x35, ProtocolConstants.Direction.TO_CLIENT, true ); // Entity Head Look : PacketPlayOutEntityHeadRotation
+        addRewrite( 0x37, ProtocolConstants.Direction.TO_CLIENT, true ); // Camera : PacketPlayOutCamera
+        addRewrite( 0x3A, ProtocolConstants.Direction.TO_CLIENT, true ); // Entity Metadata : PacketPlayOutEntityMetadata
+        addRewrite( 0x3C, ProtocolConstants.Direction.TO_CLIENT, true ); // Entity Velocity : PacketPlayOutEntityVelocity
+        addRewrite( 0x3D, ProtocolConstants.Direction.TO_CLIENT, true ); // Entity Equipment : PacketPlayOutEntityEquipment
+        addRewrite( 0x41, ProtocolConstants.Direction.TO_CLIENT, false ); // Attach Entity : PacketPlayOutMount
+        addRewrite( 0x4A, ProtocolConstants.Direction.TO_CLIENT, true ); // Collect Item : PacketPlayOutCollect
+        addRewrite( 0x4B, ProtocolConstants.Direction.TO_CLIENT, true ); // Entity Teleport : PacketPlayOutEntityTeleport
+        addRewrite( 0x4C, ProtocolConstants.Direction.TO_CLIENT, true ); // Entity Properties : PacketPlayOutUpdateAttributes
+        addRewrite( 0x4D, ProtocolConstants.Direction.TO_CLIENT, true ); // Entity Effect : PacketPlayOutEntityEffect
 
-        addRewrite( 0x02, ProtocolConstants.Direction.TO_SERVER, true ); // Use Entity
-        addRewrite( 0x0C, ProtocolConstants.Direction.TO_SERVER, true ); // Entity Action
+        addRewrite( 0x0A, ProtocolConstants.Direction.TO_SERVER, true ); // Use Entity : PacketPlayInUseEntity
+        addRewrite( 0x14, ProtocolConstants.Direction.TO_SERVER, true ); // Entity Action : PacketPlayInEntityAction
     }
 
     @Override
@@ -52,24 +51,25 @@ class EntityMap_SNAPSHOT extends EntityMap
     {
         super.rewriteClientbound( packet, oldId, newId );
 
-        //Special cases
+        // Special cases
         int readerIndex = packet.readerIndex();
         int packetId = DefinedPacket.readVarInt( packet );
         int packetIdLength = packet.readerIndex() - readerIndex;
-        if ( packetId == 0x0D /* Collect Item */ )
+        if ( packetId == 0x4A /* Collect Item : PacketPlayOutCollect */ )
         {
             DefinedPacket.readVarInt( packet );
             rewriteVarInt( packet, oldId, newId, packet.readerIndex() );
-        } else if ( packetId == 0x1B /* Attach Entity */ )
+        } else if ( packetId == 0x41 /* Attach Entity : PacketPlayOutMount */ )
         {
+            if (true) throw new RuntimeException("TODO");
             rewriteInt( packet, oldId, newId, readerIndex + packetIdLength + 4 );
-        } else if ( packetId == 0x13 /* Destroy Entities */ )
+        } else if ( packetId == 0x31 /* Destroy Entities : PacketPlayOutEntityDestroy */ )
         {
             int count = DefinedPacket.readVarInt( packet );
             int[] ids = new int[ count ];
             for ( int i = 0; i < count; i++ )
             {
-                ids[ i ] = DefinedPacket.readVarInt( packet );
+                ids[i] = DefinedPacket.readVarInt( packet );
             }
             packet.readerIndex( readerIndex + packetIdLength );
             packet.writerIndex( readerIndex + packetIdLength );
@@ -85,9 +85,9 @@ class EntityMap_SNAPSHOT extends EntityMap
                 }
                 DefinedPacket.writeVarInt( id, packet );
             }
-        } else if ( packetId == 0x0E /* Spawn Object */ )
+        } else if ( packetId == 0x00 /* Spawn Object : PacketPlayOutSpawnEntity */ )
         {
-
+            if (true) throw new RuntimeException("TODO");
             DefinedPacket.readVarInt( packet );
             int type = packet.readUnsignedByte();
 
@@ -120,7 +120,7 @@ class EntityMap_SNAPSHOT extends EntityMap
                     }
                 }
             }
-        } else if ( packetId == 0x0C /* Spawn Player */ )
+        } else if ( packetId == 0x0C /* Spawn Player : PacketPlayOutNamedEntitySpawn */ )
         {
             DefinedPacket.readVarInt( packet ); // Entity ID
             int idLength = packet.readerIndex() - readerIndex - packetIdLength;
@@ -134,7 +134,7 @@ class EntityMap_SNAPSHOT extends EntityMap
                 DefinedPacket.writeUUID( player.getUniqueId(), packet );
                 packet.writerIndex( previous );
             }
-        } else if ( packetId == 0x42 /* Combat Event */ )
+        } else if ( packetId == 0x2D /* Combat Event : PacketPlayOutCombatEvent */ )
         {
             int event = packet.readUnsignedByte();
             if ( event == 1 /* End Combat*/ )
@@ -162,7 +162,7 @@ class EntityMap_SNAPSHOT extends EntityMap
         int packetId = DefinedPacket.readVarInt( packet );
         int packetIdLength = packet.readerIndex() - readerIndex;
 
-        if ( packetId == 0x19 /* Spectate */ && !BungeeCord.getInstance().getConfig().isIpForward() )
+        if ( packetId == 0x1B /* Spectate : PacketPlayInSpectate */ && !BungeeCord.getInstance().getConfig().isIpForward() )
         {
             UUID uuid = DefinedPacket.readUUID( packet );
             ProxiedPlayer player;
