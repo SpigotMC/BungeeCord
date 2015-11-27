82f819da39759d670bfb80628ace695fc6554906
kamcio96 <k.nadworski@icloud.com>
2015-11-27 12:14:51 +0100

    Avoid disconnecting player after lost connection

--- proxy/src/main/java/net/md_5/bungee/ServerConnector.java
+++ proxy/src/main/java/net/md_5/bungee/ServerConnector.java
@@ -342,6 +342,6 @@ public class ServerConnector extends PacketHandler
     @Override
     public String toString()
     {
-        return "[" + user.getName() + "] <-> ServerConnector [" + target.getName() + "]";
+        return "[" + user.getName() + "] <-> ServerConnector <-> [" + target.getName() + "]";
     }
 }
