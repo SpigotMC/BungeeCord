0dbf4327ecdf8c3f80ed9638765ca4e7e2c7c9a9
Tux <write@imaginarycode.com>
2016-02-07 01:33:51 -0500

    Fix cancelling TabCompleteEvent.
    The packet would get sent regardless if the event was cancelled, only if there were no suggestions available.

--- proxy/src/main/java/net/md_5/bungee/connection/UpstreamBridge.java
+++ proxy/src/main/java/net/md_5/bungee/connection/UpstreamBridge.java
@@ -134,15 +134,15 @@ public class UpstreamBridge extends PacketHandler
         TabCompleteEvent tabCompleteEvent = new TabCompleteEvent( con, con.getServer(), tabComplete.getCursor(), suggestions );
         bungee.getPluginManager().callEvent( tabCompleteEvent );
 
+        if ( tabCompleteEvent.isCancelled() )
+        {
+            throw CancelSendSignal.INSTANCE;
+        }
+
         List<String> results = tabCompleteEvent.getSuggestions();
         if ( !results.isEmpty() )
         {
             con.unsafe().sendPacket( new TabCompleteResponse( results ) );
-            throw CancelSendSignal.INSTANCE;
-        }
-
-        if ( tabCompleteEvent.isCancelled() )
-        {
             throw CancelSendSignal.INSTANCE;
         }
     }
