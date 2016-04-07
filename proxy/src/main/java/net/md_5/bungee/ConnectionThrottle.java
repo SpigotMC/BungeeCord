612e58635d98381da23f31291a6f67f5e85517df
Janmm14 <computerjanimaus@yahoo.de>
2016-04-08 01:46:28 +0200

    Update the connection throttle to use a Guava cache to lower memory usage again after an attack is over

--- proxy/src/main/java/net/md_5/bungee/ConnectionThrottle.java
+++ proxy/src/main/java/net/md_5/bungee/ConnectionThrottle.java
@@ -1,25 +1,30 @@
 package net.md_5.bungee;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import java.net.InetAddress;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class ConnectionThrottle
 {
 
-    private final Map<InetAddress, Long> throttle = new HashMap<>();
     private final int throttleTime;
+    private final Cache<InetAddress, Long> throttle = CacheBuilder.newBuilder()
+            .concurrencyLevel( Runtime.getRuntime().availableProcessors() )
+            .initialCapacity( 100 )
+            .expireAfterAccess( throttleTime, TimeUnit.MILLISECONDS )
+            .build();
 
     public void unthrottle(InetAddress address)
     {
-        throttle.remove( address );
+        throttle.invalidate( address );
     }
 
     public boolean throttle(InetAddress address)
     {
-        Long value = throttle.get( address );
+        Long value = throttle.getIfPresent( address );
         long currentTime = System.currentTimeMillis();
 
         throttle.put( address, currentTime );
