b300a88efcf56cac79f76adc96641c576201aade
PunKeel <punkeel@me.com>
2016-05-15 17:17:32 +0200

    Use Guava's Ticker in ConnectionThrottle
    Used to improve tests performance : the thread is not sleeping, but the behaviour is unchanged

--- proxy/src/test/java/net/md_5/bungee/ThrottleTest.java
+++ proxy/src/test/java/net/md_5/bungee/ThrottleTest.java
@@ -1,7 +1,9 @@
 package net.md_5.bungee;
 
+import com.google.common.testing.FakeTicker;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.concurrent.TimeUnit;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -11,7 +13,8 @@ public class ThrottleTest
     @Test
     public void testThrottle() throws InterruptedException, UnknownHostException
     {
-        ConnectionThrottle throttle = new ConnectionThrottle( 10 );
+        FakeTicker ticker = new FakeTicker();
+        ConnectionThrottle throttle = new ConnectionThrottle( 10, ticker );
         InetAddress address;
 
         try
@@ -24,8 +27,14 @@ public class ThrottleTest
 
         Assert.assertFalse( "Address should not be throttled", throttle.throttle( address ) );
         Assert.assertTrue( "Address should be throttled", throttle.throttle( address ) );
+        
+        ticker.advance(6, TimeUnit.MILLISECONDS);
+        Assert.assertTrue( "Address should be throttled", throttle.throttle( address ) );
+        
+        ticker.advance(6, TimeUnit.MILLISECONDS);
+        Assert.assertTrue( "Address should be throttled again", throttle.throttle( address ) );
 
-        Thread.sleep( 50 );
-        Assert.assertFalse( "Address should not be throttled", throttle.throttle( address ) );
+        ticker.advance(11, TimeUnit.MILLISECONDS);
+        Assert.assertFalse( "Address should not be throttled anymore", throttle.throttle( address ) );
     }
 }
