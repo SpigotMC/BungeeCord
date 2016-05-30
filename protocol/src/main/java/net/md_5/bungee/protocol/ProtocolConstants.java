89ebff72c69b9ecd0fcac4c853afc528739b5398
kmh11 <hoppykev@gmail.com>
2016-05-30 13:54:17 +0000

    Add 16w21b

--- protocol/src/main/java/net/md_5/bungee/protocol/ProtocolConstants.java
+++ protocol/src/main/java/net/md_5/bungee/protocol/ProtocolConstants.java
@@ -11,15 +11,18 @@ public class ProtocolConstants
     public static final int MINECRAFT_1_9_1 = 108;
     public static final int MINECRAFT_1_9_2 = 109;
     public static final int MINECRAFT_1_9_4 = 110;
+    public static final int MINECRAFT_16w21b = 203;
     public static final List<String> SUPPORTED_VERSIONS = Arrays.asList(
             "1.8.x",
             "1.9.x"
+            "16w21b"
     );
     public static final List<Integer> SUPPORTED_VERSION_IDS = Arrays.asList(ProtocolConstants.MINECRAFT_1_8,
             ProtocolConstants.MINECRAFT_1_9,
             ProtocolConstants.MINECRAFT_1_9_1,
             ProtocolConstants.MINECRAFT_1_9_2,
-            ProtocolConstants.MINECRAFT_1_9_4
+            ProtocolConstants.MINECRAFT_1_9_4,
+            ProtocolConstants.MINECRAFT_16w21b
     );
 
     public enum Direction
