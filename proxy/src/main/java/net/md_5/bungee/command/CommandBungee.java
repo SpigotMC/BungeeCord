2997e8955523504f2645b4d01b1c0fe981208a37
SAM_XPS <bsbcraftplays@gmail.com>
2016-07-19 21:38:00 -0300

    Changing old sendMessage via String to the new ComponentBuilder

--- proxy/src/main/java/net/md_5/bungee/command/CommandBungee.java
+++ proxy/src/main/java/net/md_5/bungee/command/CommandBungee.java
@@ -3,6 +3,7 @@ package net.md_5.bungee.command;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.plugin.Command;
 
 public class CommandBungee extends Command
@@ -16,6 +17,6 @@ public class CommandBungee extends Command
     @Override
     public void execute(CommandSender sender, String[] args)
     {
-        sender.sendMessage( ChatColor.BLUE + "This server is running BungeeCord version " + ProxyServer.getInstance().getVersion() + " by md_5" );
+        sender.sendMessage( new ComponentBuilder( "This server is running BungeeCord version " + ProxyServer.getInstance().getVersion() + " by md_5" ).color( ChatColor.BLUE ).create() );
     }
 }
