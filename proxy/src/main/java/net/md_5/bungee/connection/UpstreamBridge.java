e7be3c6b1e58f89759941315e203470991a0b711
md_5 <git@md-5.net>
2016-08-11 23:02:29 +1000

    Allow PluginMessages to be handled when no server connected

--- proxy/src/main/java/net/md_5/bungee/connection/UpstreamBridge.java
+++ proxy/src/main/java/net/md_5/bungee/connection/UpstreamBridge.java
@@ -83,7 +83,7 @@ public class UpstreamBridge extends PacketHandler
     @Override
     public boolean shouldHandle(PacketWrapper packet) throws Exception
     {
-        return con.getServer() != null;
+        return con.getServer() != null || packet.packet instanceof PluginMessage;
     }
 
     @Override
