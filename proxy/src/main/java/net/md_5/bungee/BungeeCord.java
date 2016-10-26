b7d39f89e022e032f6d1b031507e60d1e1946ddd
AlfieC <AlfieC@users.noreply.github.com>
2016-10-26 01:19:54 +0100

    Code style warrants a newline

--- proxy/src/main/java/net/md_5/bungee/BungeeCord.java
+++ proxy/src/main/java/net/md_5/bungee/BungeeCord.java
@@ -275,6 +275,7 @@ public class BungeeCord extends ProxyServer
         {
             connectionThrottle = new ConnectionThrottle( config.getThrottle() );
         }
+
         startListeners();
 
         saveThread.scheduleAtFixedRate( new TimerTask()
