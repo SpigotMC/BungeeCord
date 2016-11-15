8574688be7512040fa2c75e101ee6f17e5444e6b
Nathan Poirier <nathan@poirier.io>
2016-11-15 23:22:09 +0100

    Fix compatibility of Title packet for 1.10 and lower (#1997)

--- protocol/src/main/java/net/md_5/bungee/protocol/packet/Title.java
+++ protocol/src/main/java/net/md_5/bungee/protocol/packet/Title.java
@@ -31,7 +31,7 @@ public class Title extends DefinedPacket
         int index = readVarInt( buf );
 
         // If we're working on 1.10 or lower, increment the value of the index so we pull out the correct value.
-        if ( protocolVersion <= ProtocolConstants.MINECRAFT_1_10 && index <= 2 )
+        if ( protocolVersion <= ProtocolConstants.MINECRAFT_1_10 && index >= 2 )
         {
             index++;
         }
@@ -58,9 +58,9 @@ public class Title extends DefinedPacket
         int index = action.ordinal();
 
         // If we're working on 1.10 or lower, increment the value of the index so we pull out the correct value.
-        if ( protocolVersion <= ProtocolConstants.MINECRAFT_1_10 && index <= 2 )
+        if ( protocolVersion <= ProtocolConstants.MINECRAFT_1_10 && index >= 2 )
         {
-            index++;
+            index--;
         }
 
         writeVarInt( index, buf );
