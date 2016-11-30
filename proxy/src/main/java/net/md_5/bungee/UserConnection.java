2171ca9f51ad5943f8aa90db1094bdc52fc42399
md_5 <git@md-5.net>
2016-12-01 09:46:34 +1100

    Fix confusing use of static subclass methods

--- proxy/src/main/java/net/md_5/bungee/UserConnection.java
+++ proxy/src/main/java/net/md_5/bungee/UserConnection.java
@@ -431,7 +431,7 @@ public final class UserConnection implements ProxiedPlayer
         // Action bar on 1.8 doesn't display the new JSON formattings, legacy works - send it using this for now
         if ( position == ChatMessageType.ACTION_BAR && getPendingConnection().getVersion() <= ProtocolConstants.MINECRAFT_1_8 )
         {
-            sendMessage( position, ComponentSerializer.toString( new TextComponent( TextComponent.toLegacyText( message ) ) ) );
+            sendMessage( position, ComponentSerializer.toString( new TextComponent( BaseComponent.toLegacyText( message ) ) ) );
         } else
         {
             sendMessage( position, ComponentSerializer.toString( message ) );
@@ -444,7 +444,7 @@ public final class UserConnection implements ProxiedPlayer
         // Action bar on 1.8 doesn't display the new JSON formattings, legacy works - send it using this for now
         if ( position == ChatMessageType.ACTION_BAR && getPendingConnection().getVersion() <= ProtocolConstants.MINECRAFT_1_8 )
         {
-            sendMessage( position, ComponentSerializer.toString( new TextComponent( TextComponent.toLegacyText( message ) ) ) );
+            sendMessage( position, ComponentSerializer.toString( new TextComponent( BaseComponent.toLegacyText( message ) ) ) );
         } else
         {
             sendMessage( position, ComponentSerializer.toString( message ) );
