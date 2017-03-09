795aa04150dc1e4bc1bb9fd328d893754736b7ab
Dragovorn <andrewburr011@gmail.com>
2017-03-09 08:48:09 -0500

    removed lombok constructor and replaced it with a VarArgs constructor

--- chat/src/main/java/net/md_5/bungee/api/chat/HoverEvent.java
+++ chat/src/main/java/net/md_5/bungee/api/chat/HoverEvent.java
@@ -1,17 +1,20 @@
 package net.md_5.bungee.api.chat;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 @Getter
 @ToString
-@RequiredArgsConstructor
 final public class HoverEvent
 {
 
     private final Action action;
     private final BaseComponent[] value;
+
+    public HoverEvent(Action action, BaseComponent... value) {
+        this.action = action;
+        this.value = value;
+    }
 
     public enum Action
     {
