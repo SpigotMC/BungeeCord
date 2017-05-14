16d261553c1a648ce38101bda19b7166d1ca5a41
md_5 <git@md-5.net>
2017-05-14 12:00:00 +1000

    Minecraft 1.12-pre2 Support

--- protocol/src/main/java/net/md_5/bungee/protocol/ProtocolConstants.java
+++ protocol/src/main/java/net/md_5/bungee/protocol/ProtocolConstants.java
@@ -14,11 +14,13 @@ public class ProtocolConstants
     public static final int MINECRAFT_1_10 = 210;
     public static final int MINECRAFT_1_11 = 315;
     public static final int MINECRAFT_1_11_1 = 316;
+    public static final int MINECRAFT_1_12 = 329;
     public static final List<String> SUPPORTED_VERSIONS = Arrays.asList(
             "1.8.x",
             "1.9.x",
             "1.10.x",
-            "1.11.x"
+            "1.11.x",
+            "1.12.x"
     );
     public static final List<Integer> SUPPORTED_VERSION_IDS = Arrays.asList( ProtocolConstants.MINECRAFT_1_8,
             ProtocolConstants.MINECRAFT_1_9,
@@ -27,7 +29,8 @@ public class ProtocolConstants
             ProtocolConstants.MINECRAFT_1_9_4,
             ProtocolConstants.MINECRAFT_1_10,
             ProtocolConstants.MINECRAFT_1_11,
-            ProtocolConstants.MINECRAFT_1_11_1
+            ProtocolConstants.MINECRAFT_1_11_1,
+            ProtocolConstants.MINECRAFT_1_12
     );
 
     public enum Direction
