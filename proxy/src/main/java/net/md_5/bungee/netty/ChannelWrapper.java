33824aae2e8480c50740e87142de619c58a3907e
Dimatert9 <mikimouse100@mail.ru>
2017-06-24 22:30:06 +0300

    oops

--- proxy/src/main/java/net/md_5/bungee/netty/ChannelWrapper.java
+++ proxy/src/main/java/net/md_5/bungee/netty/ChannelWrapper.java
@@ -43,7 +43,7 @@ public class ChannelWrapper
 
     public void write(Object packet)
     {
-        if ( !closed )
+        if ( !closed && ch.isWritable() )
         {
             if ( packet instanceof PacketWrapper )
             {
