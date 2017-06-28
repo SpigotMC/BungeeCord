a5ffeae757d9fbb81efa87b1bae49ca5175f4007
md_5 <git@md-5.net>
2017-06-29 08:52:13 +1000

    Replace OpenSSL native cipher with static mbed TLS for maximum compat

--- proxy/src/main/java/net/md_5/bungee/BungeeCord.java
+++ proxy/src/main/java/net/md_5/bungee/BungeeCord.java
@@ -220,17 +220,17 @@ public class BungeeCord extends ProxyServer
         {
             if ( EncryptionUtil.nativeFactory.load() )
             {
-                logger.info( "Using OpenSSL based native cipher." );
+                logger.info( "Using mbed TLS based native cipher." );
             } else
             {
-                logger.info( "Using standard Java JCE cipher. To enable the OpenSSL based native cipher, please make sure you are using 64 bit Ubuntu or Debian with libssl installed." );
+                logger.info( "Using standard Java JCE cipher." );
             }
             if ( CompressFactory.zlib.load() )
             {
-                logger.info( "Using native code compressor" );
+                logger.info( "Using zlib based native compressor." );
             } else
             {
-                logger.info( "Using standard Java compressor. To enable zero copy compression, run on 64 bit Linux" );
+                logger.info( "Using standard Java compressor." );
             }
         }
     }
