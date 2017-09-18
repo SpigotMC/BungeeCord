8365a41933f5edbe91d677620825ff922bddee0b
Dimatert9 <mikimouse100@mail.ru>
2017-09-18 20:11:36 +0300

    new version

--- bootstrap/src/main/java/net/md_5/bungee/BungeeCordLauncher.java
+++ bootstrap/src/main/java/net/md_5/bungee/BungeeCordLauncher.java
@@ -23,7 +23,7 @@ import net.md_5.bungee.command.ConsoleCommandSender;
 public class BungeeCordLauncher
 {
 
-    private static int VERSION = 19;
+    private static int VERSION = 191;
 
     public static void main(String[] args) throws Exception
     {
