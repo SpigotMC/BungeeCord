fbc5f514e28dbfc3f85cb936ad95b1a979086ab6
BlackHole <black-hole@live.com>
2017-11-10 11:14:04 +1100

    Add KeybindComponent API

---
+++ chat/src/main/java/net/md_5/bungee/api/chat/Keybinds.java
@@ -0,0 +1,51 @@
+package net.md_5.bungee.api.chat;
+
+/**
+ * All keybind values supported by vanilla Minecraft.
+ * <br>
+ * Values may be removed if they are no longer supported.
+ *
+ * @see KeybindComponent
+ */
+public interface Keybinds
+{
+
+    String JUMP = "key.jump";
+    String SNEAK = "key.sneak";
+    String SPRINT = "key.sprint";
+    String LEFT = "key.left";
+    String RIGHT = "key.right";
+    String BACK = "key.back";
+    String FORWARD = "key.forward";
+
+    String ATTACK = "key.attack";
+    String PICK_ITEM = "key.pickItem";
+    String USE = "key.use";
+
+    String DROP = "key.drop";
+    String HOTBAR_1 = "key.hotbar.1";
+    String HOTBAR_2 = "key.hotbar.2";
+    String HOTBAR_3 = "key.hotbar.3";
+    String HOTBAR_4 = "key.hotbar.4";
+    String HOTBAR_5 = "key.hotbar.5";
+    String HOTBAR_6 = "key.hotbar.6";
+    String HOTBAR_7 = "key.hotbar.7";
+    String HOTBAR_8 = "key.hotbar.8";
+    String HOTBAR_9 = "key.hotbar.9";
+    String INVENTORY = "key.inventory";
+    String SWAP_HANDS = "key.swapHands";
+
+    String LOAD_TOOLBAR_ACTIVATOR = "key.loadToolbarActivator";
+    String SAVE_TOOLBAR_ACTIVATOR = "key.saveToolbarActivator";
+
+    String PLAYERLIST = "key.playerlist";
+    String CHAT = "key.chat";
+    String COMMAND = "key.command";
+
+    String ADVANCEMENTS = "key.advancements";
+    String SPECTATOR_OUTLINES = "key.spectatorOutlines";
+    String SCREENSHOT = "key.screenshot";
+    String SMOOTH_CAMERA = "key.smoothCamera";
+    String FULLSCREEN = "key.fullscreen";
+    String TOGGLE_PERSPECTIVE = "key.togglePerspective";
+}
