d600c9a526ba9234e84150aad959079ac278f635
md_5 <git@md-5.net>
2018-01-08 12:50:53 +1100

    #2323: Attempt to rewrite fishing hook data

--- proxy/src/main/java/net/md_5/bungee/entitymap/EntityMap_1_12_1.java
+++ proxy/src/main/java/net/md_5/bungee/entitymap/EntityMap_1_12_1.java
@@ -149,6 +149,7 @@ class EntityMap_1_12_1 extends EntityMap
                 break;
             case 0x3C /* EntityMetadata : PacketPlayOutEntityMetadata */:
                 DefinedPacket.readVarInt( packet ); // Entity ID
+                rewriteMetaVarInt( packet, oldId + 1, newId + 1, 6 ); // fishing hook
                 rewriteMetaVarInt( packet, oldId, newId, 7 ); // fireworks (et al)
                 break;
         }
