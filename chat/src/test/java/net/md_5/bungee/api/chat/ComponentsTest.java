ca8f31bdc70a2ec626274b4659b99732ec602969
md_5 <git@md-5.net>
2018-01-28 18:00:24 +1100

    Merge unit tests

--- chat/src/test/java/net/md_5/bungee/api/chat/ComponentsTest.java
+++ chat/src/test/java/net/md_5/bungee/api/chat/ComponentsTest.java
@@ -191,4 +191,34 @@ public class ComponentsTest
         a.addExtra( b );
         ComponentSerializer.toString( a );
     }
+
+    @Test
+    public void testInvalidColorCodes()
+    {
+        StringBuilder allInvalidColorCodes = new StringBuilder();
+
+        // collect all invalid color codes (e.g. §z, §g, ...)
+        for ( char alphChar : "0123456789abcdefghijklmnopqrstuvwxyz".toCharArray() )
+        {
+            if ( ChatColor.ALL_CODES.indexOf( alphChar ) == -1 )
+            {
+                allInvalidColorCodes.append( ChatColor.COLOR_CHAR );
+                allInvalidColorCodes.append( alphChar );
+            }
+        }
+
+        // last char is a single '§'
+        allInvalidColorCodes.append( ChatColor.COLOR_CHAR );
+
+        String invalidColorCodesLegacyText = fromAndToLegacyText( allInvalidColorCodes.toString() );
+        String emptyLegacyText = fromAndToLegacyText( "" );
+
+        // all invalid color codes and the trailing '§' should be ignored
+        Assert.assertEquals( emptyLegacyText, invalidColorCodesLegacyText );
+    }
+
+    private String fromAndToLegacyText(String legacyText)
+    {
+        return BaseComponent.toLegacyText( TextComponent.fromLegacyText( legacyText ) );
+    }
 }
