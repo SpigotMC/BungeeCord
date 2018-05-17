80ff37020912aa8cc793b3126dfdcb97cda7a492
Dimatert9 <mikimouse100@mail.ru>
2018-05-17 19:47:10 +0300

    increase ping limiter threshold

--- proxy/src/main/java/ru/leymooo/botfilter/utils/PingLimiter.java
+++ proxy/src/main/java/ru/leymooo/botfilter/utils/PingLimiter.java
@@ -14,7 +14,7 @@ public class PingLimiter
 
     private static final long ONE_MIN = 60000;
     private static final long BAN_TIME = ONE_MIN * 5;
-    private static int THRESHOLD = 750;
+    private static int THRESHOLD = 950;
     private static long LASTCHECK = System.currentTimeMillis();
     private static int currNumOfPings = 0;
     private static boolean banned = false;
