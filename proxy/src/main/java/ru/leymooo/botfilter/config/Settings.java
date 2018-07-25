053284abd196e77a20c4ac433b104249416c0be7
Leymooo <mikimouse100@mail.ru>
2018-07-25 17:32:04 +0300

    wrong version

--- proxy/src/main/java/ru/leymooo/botfilter/config/Settings.java
+++ proxy/src/main/java/ru/leymooo/botfilter/config/Settings.java
@@ -20,7 +20,7 @@ public class Settings extends Config
     @Final
     public final String HELP = "http://www.rubukkit.org/threads/137038/";
     @Final
-    public String BOT_FILTER_VERSION = "3.3.5";
+    public String BOT_FILTER_VERSION = "3.3.6";
 
     @Create
     public MESSAGES MESSAGES;
