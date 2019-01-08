54ce8b3096f8fc4db9268660078f23e243d56118
Simon Chuu <simonorj@outlook.com>
2019-01-07 19:41:33 -0500

    ChatEvent: Add ChatEvent.isProxyCommand()
    This new method checks if the said command is run on the proxy itself.

--- api/src/main/java/net/md_5/bungee/api/event/ChatEvent.java
+++ api/src/main/java/net/md_5/bungee/api/event/ChatEvent.java
@@ -3,8 +3,13 @@ package net.md_5.bungee.api.event;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.connection.Connection;
 import net.md_5.bungee.api.plugin.Cancellable;
+import net.md_5.bungee.api.plugin.Command;
+
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * Event called when a player sends a message to a server.
@@ -38,5 +43,40 @@ public class ChatEvent extends TargetedEvent implements Cancellable
     public boolean isCommand()
     {
         return message.length() > 0 && message.charAt( 0 ) == '/';
+    }
+
+    /**
+     * Checks whether this message is run on this proxy server
+     *
+     * @return if this command runs on the proxy
+     */
+    public boolean isProxyCommand()
+    {
+        if (!isCommand())
+        {
+            return false;
+        }
+
+        String[] split = message.split( " ", -1 );
+        // Check for chat that only contains " "
+        if ( split.length == 0 || split[0].isEmpty() )
+        {
+            return false;
+        }
+
+        ProxyServer proxy = ProxyServer.getInstance();
+        String commandName = split[0].toLowerCase( Locale.ROOT );
+        if ( ProxyServer.getInstance().getDisabledCommands().contains( commandName ) )
+        {
+            return false;
+        }
+
+        for ( Map.Entry<String, Command> e : proxy.getPluginManager().getCommands() )
+        {
+            if ( e.getKey().equalsIgnoreCase( commandName ) )
+                return true;
+        }
+
+        return false;
     }
 }
