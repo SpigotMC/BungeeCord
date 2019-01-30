219d55dfda6764aec56478e19ba00c002fa8094e
md_5 <git@md-5.net>
2019-01-30 15:45:47 +1100

    #2592: Fix null tooltip string

--- protocol/src/main/java/net/md_5/bungee/protocol/packet/TabCompleteResponse.java
+++ protocol/src/main/java/net/md_5/bungee/protocol/packet/TabCompleteResponse.java
@@ -79,7 +79,7 @@ public class TabCompleteResponse extends DefinedPacket
             {
                 writeString( suggestion.getText(), buf );
                 buf.writeBoolean( suggestion.getTooltip() != null );
-                if ( suggestion.getTooltip() != null )
+                if ( suggestion.getTooltip() != null && suggestion.getTooltip().getString() != null )
                 {
                     writeString( suggestion.getTooltip().getString(), buf );
                 }
