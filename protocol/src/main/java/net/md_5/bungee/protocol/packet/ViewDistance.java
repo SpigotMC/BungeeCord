59e82c13631fdedf0a81a5702542be517db842cb
riku6460 <17585784+riku6460@users.noreply.github.com>
2019-04-28 15:44:47 +0900

    Fix formatting

--- protocol/src/main/java/net/md_5/bungee/protocol/packet/ViewDistance.java
+++ protocol/src/main/java/net/md_5/bungee/protocol/packet/ViewDistance.java
@@ -14,21 +14,21 @@ import net.md_5.bungee.protocol.DefinedPacket;
 @EqualsAndHashCode(callSuper = false)
 public class ViewDistance extends DefinedPacket
 {
-    
+
     private int distance;
-    
+
     @Override
     public void read(ByteBuf buf)
     {
         distance = DefinedPacket.readVarInt( buf );
     }
-    
+
     @Override
     public void write(ByteBuf buf)
     {
         DefinedPacket.writeVarInt( distance, buf );
     }
-    
+
     @Override
     public void handle(AbstractPacketHandler handler) throws Exception
     {
