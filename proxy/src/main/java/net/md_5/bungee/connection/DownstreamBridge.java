4f0a2a8d1f4ce0f5ef5dfc6928f1a88ee11f4d4a
Mystiflow <mystiflow@gmail.com>
2019-12-02 19:35:38 +0000

    Map scoreboard names to display slots
    Previously BungeeCord would only know the name and position of the most recently sent scoreboard by the server
    
    With this commit BungeeCord tracks the names for each display slot in the scoreboard (list, sidebar, position, colors)

--- proxy/src/main/java/net/md_5/bungee/connection/DownstreamBridge.java
+++ proxy/src/main/java/net/md_5/bungee/connection/DownstreamBridge.java
@@ -181,8 +181,7 @@ public class DownstreamBridge extends PacketHandler
     public void handle(ScoreboardDisplay displayScoreboard) throws Exception
     {
         Scoreboard serverScoreboard = con.getServerSentScoreboard();
-        serverScoreboard.setName( displayScoreboard.getName() );
-        serverScoreboard.setPosition( Position.values()[displayScoreboard.getPosition()] );
+        serverScoreboard.updateName( Position.values()[displayScoreboard.getPosition()], displayScoreboard.getName() );
     }
 
     @Override
