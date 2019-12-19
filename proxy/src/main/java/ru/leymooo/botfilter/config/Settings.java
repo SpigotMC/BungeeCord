87f0e72dd2c50d6bab97a3143b75db5ef25f619a
Leymooo <mikimouse100@mail.ru>
2019-12-19 10:40:43 +0200

    [3.7.8-2]

--- proxy/src/main/java/ru/leymooo/botfilter/config/Settings.java
+++ proxy/src/main/java/ru/leymooo/botfilter/config/Settings.java
@@ -20,7 +20,7 @@ public class Settings extends Config
     @Final
     public final String HELP = "http://www.rubukkit.org/threads/137038/";
     @Final
-    public String BOT_FILTER_VERSION = "3.7.8";
+    public String BOT_FILTER_VERSION = "3.7.8-2";
 
     @Create
     public MESSAGES MESSAGES;
