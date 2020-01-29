bf673c5d8b965a035ab55753cfbc6038f8a4cfbb
md_5 <git@md-5.net>
2020-01-29 12:03:22 +1100

    Add pretty colours to console log levels

--- log/src/main/java/net/md_5/bungee/log/ConciseFormatter.java
+++ log/src/main/java/net/md_5/bungee/log/ConciseFormatter.java
@@ -5,12 +5,17 @@ import java.io.StringWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.logging.Formatter;
+import java.util.logging.Level;
 import java.util.logging.LogRecord;
+import lombok.RequiredArgsConstructor;
+import net.md_5.bungee.api.ChatColor;
 
+@RequiredArgsConstructor
 public class ConciseFormatter extends Formatter
 {
 
     private final DateFormat date = new SimpleDateFormat( System.getProperty( "net.md_5.bungee.log-date-format", "HH:mm:ss" ) );
+    private final boolean coloured;
 
     @Override
     @SuppressWarnings("ThrowableResultIgnored")
@@ -20,7 +25,7 @@ public class ConciseFormatter extends Formatter
 
         formatted.append( date.format( record.getMillis() ) );
         formatted.append( " [" );
-        formatted.append( record.getLevel().getLocalizedName() );
+        appendLevel( formatted, record.getLevel() );
         formatted.append( "] " );
         formatted.append( formatMessage( record ) );
         formatted.append( '\n' );
@@ -33,5 +38,32 @@ public class ConciseFormatter extends Formatter
         }
 
         return formatted.toString();
+    }
+
+    private void appendLevel(StringBuilder builder, Level level)
+    {
+        if ( !coloured )
+        {
+            builder.append( level.getLocalizedName() );
+            return;
+        }
+
+        ChatColor color;
+
+        if ( level == Level.INFO )
+        {
+            color = ChatColor.BLUE;
+        } else if ( level == Level.WARNING )
+        {
+            color = ChatColor.YELLOW;
+        } else if ( level == Level.SEVERE )
+        {
+            color = ChatColor.RED;
+        } else
+        {
+            color = ChatColor.AQUA;
+        }
+
+        builder.append( color ).append( level.getLocalizedName() ).append( ChatColor.RESET );
     }
 }
