f41b1fc82131842409fed2df1c702cda36b06bd2
md_5 <git@md-5.net>
2020-02-20 12:24:01 +1100

    Do not allow plugins to make multiple instances of their main class.
    Prevents foot shooting, eg #2772

--- api/src/main/java/net/md_5/bungee/api/plugin/PluginClassloader.java
+++ api/src/main/java/net/md_5/bungee/api/plugin/PluginClassloader.java
@@ -1,37 +1,47 @@
 package net.md_5.bungee.api.plugin;
 
+import com.google.common.base.Preconditions;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
+import net.md_5.bungee.api.ProxyServer;
 
-public class PluginClassloader extends URLClassLoader
+final class PluginClassloader extends URLClassLoader
 {
 
     private static final Set<PluginClassloader> allLoaders = new CopyOnWriteArraySet<>();
+    //
+    private final ProxyServer proxy;
+    private final PluginDescription desc;
+    //
+    private Plugin plugin;
 
     static
     {
         ClassLoader.registerAsParallelCapable();
     }
 
-    public PluginClassloader(URL[] urls)
+    public PluginClassloader(ProxyServer proxy, PluginDescription desc, URL[] urls)
     {
         super( urls );
+        this.proxy = proxy;
+        this.desc = desc;
+
         allLoaders.add( this );
     }
 
     @Override
-    protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException
+    protected Class<?> findClass(String name) throws ClassNotFoundException
     {
-        return loadClass0( name, resolve, true );
+        return findClass0( name, true );
     }
 
-    private Class<?> loadClass0(String name, boolean resolve, boolean checkOther) throws ClassNotFoundException
+    private Class<?> findClass0(String name, boolean checkOther) throws ClassNotFoundException
     {
         try
         {
-            return super.loadClass( name, resolve );
+            return super.findClass( name );
         } catch ( ClassNotFoundException ex )
         {
         }
@@ -43,7 +53,7 @@ public class PluginClassloader extends URLClassLoader
                 {
                     try
                     {
-                        return loader.loadClass0( name, resolve, false );
+                        return loader.findClass0( name, false );
                     } catch ( ClassNotFoundException ex )
                     {
                     }
@@ -51,5 +61,18 @@ public class PluginClassloader extends URLClassLoader
             }
         }
         throw new ClassNotFoundException( name );
+    }
+
+    void init(Plugin plugin)
+    {
+        Preconditions.checkArgument( plugin != null, "plugin" );
+        Preconditions.checkArgument( plugin.getClass().getClassLoader() == this, "Plugin has incorrect ClassLoader" );
+        if ( this.plugin != null )
+        {
+            throw new IllegalArgumentException( "Plugin already initialized!" );
+        }
+
+        this.plugin = plugin;
+        plugin.init( proxy, desc );
     }
 }
