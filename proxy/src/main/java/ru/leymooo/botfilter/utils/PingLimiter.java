6a03a770100ae8bb9a9cd0f986c3240231500286
Leymooo <mikimouse100@mail.ru>
2020-05-04 04:20:59 +0300

    allow change ping limit using system property

--- proxy/src/main/java/ru/leymooo/botfilter/utils/PingLimiter.java
+++ proxy/src/main/java/ru/leymooo/botfilter/utils/PingLimiter.java
@@ -13,7 +13,7 @@ public final class PingLimiter
 
     private static final long ONE_MIN = 60000;
     private static final long BAN_TIME = ONE_MIN * 5;
-    private static int THRESHOLD = 950;
+    private static int THRESHOLD = Integer.getInteger( "pingThreshold", 1000 );
     private static long LASTCHECK = System.currentTimeMillis();
     private static int currNumOfPings = 0;
     private static boolean banned = false;
