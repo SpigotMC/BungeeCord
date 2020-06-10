2955398c16817f04a05e44d4a9b00922acdda6a7
LinuxServer <Jerome.Wolff1207@gmail.com>
2020-06-10 20:36:31 +0200

    Add streams in PlayerCommand

--- api/src/main/java/net/md_5/bungee/command/PlayerCommand.java
+++ api/src/main/java/net/md_5/bungee/command/PlayerCommand.java
@@ -1,12 +1,9 @@
 package net.md_5.bungee.command;
 
-import com.google.common.base.Function;
-import com.google.common.base.Predicate;
-import com.google.common.collect.Iterables;
 import java.util.Locale;
+import java.util.stream.Collectors;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.TabExecutor;
 
@@ -30,21 +27,12 @@ public abstract class PlayerCommand extends Command implements TabExecutor
     @Override
     public Iterable<String> onTabComplete(CommandSender sender, String[] args)
     {
-        final String lastArg = ( args.length > 0 ) ? args[args.length - 1].toLowerCase( Locale.ROOT ) : "";
-        return Iterables.transform( Iterables.filter( ProxyServer.getInstance().getPlayers(), new Predicate<ProxiedPlayer>()
-        {
-            @Override
-            public boolean apply(ProxiedPlayer player)
-            {
-                return player.getName().toLowerCase( Locale.ROOT ).startsWith( lastArg );
-            }
-        } ), new Function<ProxiedPlayer, String>()
-        {
-            @Override
-            public String apply(ProxiedPlayer player)
-            {
-                return player.getName();
-            }
-        } );
+        final String lastArg = (args.length > 0) ? args[args.length - 1].toLowerCase(Locale.ROOT) : "";
+        return ProxyServer.getInstance().getPlayers().stream()
+            .filter(player -> player.getName().toLowerCase(Locale.ROOT).startsWith(lastArg))
+            .collect(Collectors.toList())
+            .stream()
+            .map(CommandSender::getName)
+            .collect(Collectors.toList());
     }
 }
