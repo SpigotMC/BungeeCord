bcc3460dda3dd908b24ff3337c87ea1857b19268
Mystiflow <mystiflow@gmail.com>
2020-07-07 09:08:04 +1000

    #2893: Refactored Hover Code for Maintainability

--- chat/src/main/java/net/md_5/bungee/api/chat/ItemTag.java
+++ chat/src/main/java/net/md_5/bungee/api/chat/ItemTag.java
@@ -1,20 +1,18 @@
 package net.md_5.bungee.api.chat;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.Singular;
@@ -23,23 +21,32 @@ import lombok.ToString;
 /**
  * Metadata for use in conjunction with {@link HoverEvent.Action#SHOW_ITEM}
  */
-@ToString(callSuper = true)
+@Builder(builderClassName = "Builder", access = AccessLevel.PRIVATE)
+@ToString(of = "nbt")
+@EqualsAndHashCode(of = "nbt")
 @Setter
-@Builder(builderClassName = "Builder", access = AccessLevel.PUBLIC)
 @AllArgsConstructor
-@EqualsAndHashCode
 public final class ItemTag
 {
 
+    @Getter
+    private final String nbt;
+
     private BaseComponent name;
     @Singular("ench")
-    private List<Enchantment> enchantments = new ArrayList<>();
+    private List<Enchantment> enchantments;
     @Singular("lore")
-    private List<BaseComponent[]> lore = new ArrayList<>();
+    private List<BaseComponent[]> lore;
     private Boolean unbreakable;
 
-    private ItemTag()
+    private ItemTag(String nbt)
     {
+        this.nbt = nbt;
+    }
+
+    public static ItemTag ofNbt(String nbt)
+    {
+        return new ItemTag( nbt );
     }
 
     @RequiredArgsConstructor
@@ -56,103 +63,13 @@ public final class ItemTag
         @Override
         public ItemTag deserialize(JsonElement element, Type type, JsonDeserializationContext context) throws JsonParseException
         {
-            ItemTag itemTag = new ItemTag();
-            JsonObject object = element.getAsJsonObject();
-            if ( object.has( "ench" ) )
-            {
-                for ( JsonElement jsonElement : object.get( "ench" ).getAsJsonArray() )
-                {
-                    JsonObject next = jsonElement.getAsJsonObject();
-                    itemTag.enchantments.add( new Enchantment( next.get( "id" ).getAsInt(), next.get( "lvl" ).getAsInt() ) );
-                }
-            }
-            if ( object.has( "Unbreakable" ) )
-            {
-                int status = object.get( "Unbreakable" ).getAsInt();
-                if ( status == 1 )
-                {
-                    itemTag.unbreakable = true;
-                } else if ( status == 0 )
-                {
-                    itemTag.unbreakable = false;
-                }
-            }
-            if ( object.has( "display" ) )
-            {
-                JsonObject display = object.get( "display" ).getAsJsonObject();
-                if ( display.has( "Name" ) )
-                {
-                    itemTag.name = context.deserialize( display.get( "Name" ).getAsJsonObject(), BaseComponent.class );
-                }
-
-                if ( display.has( "Lore" ) )
-                {
-                    JsonElement lore = display.get( "Lore" );
-                    if ( lore.isJsonArray() )
-                    {
-                        for ( JsonElement loreIt : lore.getAsJsonArray() )
-                        {
-                            if ( loreIt.isJsonArray() )
-                            {
-                                itemTag.lore.add( context.deserialize( loreIt, BaseComponent[].class ) );
-                            } else
-                            {
-                                itemTag.lore.add( new BaseComponent[]
-                                {
-                                    context.deserialize( loreIt, BaseComponent.class )
-                                } );
-                            }
-                        }
-                    } else
-                    {
-                        itemTag.lore.add( context.deserialize( display.get( "Lore" ), BaseComponent[].class ) );
-                    }
-                }
-            }
-            return itemTag;
+            return ofNbt( element.toString().replace( "\"", "" ) );
         }
 
         @Override
         public JsonElement serialize(ItemTag itemTag, Type type, JsonSerializationContext context)
         {
-            JsonObject object = new JsonObject();
-
-            if ( !itemTag.enchantments.isEmpty() )
-            {
-                JsonArray enchArray = new JsonArray();
-                for ( Enchantment ench : itemTag.enchantments )
-                {
-                    JsonObject enchObj = new JsonObject();
-                    enchObj.addProperty( "id", ench.id );
-                    enchObj.addProperty( "lvl", ench.level );
-                    enchArray.add( enchObj );
-                }
-                object.add( "ench", enchArray );
-            }
-
-            if ( itemTag.unbreakable != null )
-            {
-                object.addProperty( "Unbreakable", ( itemTag.unbreakable ) ? 1 : 0 );
-            }
-
-            JsonObject display = new JsonObject();
-
-            if ( itemTag.name != null )
-            {
-                display.add( "Name", context.serialize( itemTag.name ) );
-            }
-
-            if ( !itemTag.lore.isEmpty() )
-            {
-                display.add( "Lore", context.serialize( itemTag.lore ) );
-            }
-
-            if ( display.size() != 0 )
-            {
-                object.add( "display", display );
-            }
-
-            return object;
+            return context.serialize( itemTag.getNbt() );
         }
     }
 }
--- chat/src/test/java/net/md_5/bungee/api/chat/ComponentsTest.java
+++ chat/src/test/java/net/md_5/bungee/api/chat/ComponentsTest.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.hover.content.Item;
+import net.md_5.bungee.api.chat.hover.content.Text;
 import net.md_5.bungee.chat.ComponentSerializer;
 import org.junit.Assert;
 import org.junit.Test;
@@ -20,6 +22,19 @@ public class ComponentsTest
         String serialised = ComponentSerializer.toString( component );
         BaseComponent[] deserialised = ComponentSerializer.parse( serialised );
         Assert.assertEquals( TextComponent.toLegacyText( deserialised ), TextComponent.toLegacyText( component ) );
+        //////////
+        TextComponent component1 = new TextComponent( "HoverableText" );
+        String nbt = "{display:{Name:{text:Hello},Lore:[{text:Line_1},{text:Line_2}]},ench:[{id:49,lvl:5}],Unbreakable:1}}";
+        Item contentItem = new Item( "minecraft:wood", 1, ItemTag.ofNbt( nbt ) );
+        HoverEvent hoverEvent = new HoverEvent( HoverEvent.Action.SHOW_ITEM, contentItem );
+        component1.setHoverEvent( hoverEvent );
+        json = ComponentSerializer.toString( component1 );
+        component = ComponentSerializer.parse( json );
+        Item parsedContentItem = ( (Item) component[0].getHoverEvent().getContents().get( 0 ) );
+        Assert.assertEquals( contentItem, parsedContentItem );
+        Assert.assertEquals( contentItem.getCount(), parsedContentItem.getCount() );
+        Assert.assertEquals( contentItem.getId(), parsedContentItem.getId() );
+        Assert.assertEquals( nbt, parsedContentItem.getTag().getNbt() );
     }
 
     @Test
@@ -134,6 +149,7 @@ public class ComponentsTest
         );
     }
 
+    /*
     @Test
     public void testItemTag()
     {
@@ -154,6 +170,7 @@ public class ComponentsTest
         BaseComponent[] deserialised = ComponentSerializer.parse( serialised );
         Assert.assertEquals( TextComponent.toLegacyText( deserialised ), TextComponent.toLegacyText( component ) );
     }
+     */
 
     @Test
     public void testModernShowAdvancement()
@@ -162,13 +179,13 @@ public class ComponentsTest
         // First do the text using the newer contents system
         HoverEvent hoverEvent = new HoverEvent(
                 HoverEvent.Action.SHOW_TEXT,
-                new HoverEvent.ContentText( advancement )
+                new Text( advancement )
         );
         TextComponent component = new TextComponent( "test" );
         component.setHoverEvent( hoverEvent );
         Assert.assertEquals( component.getHoverEvent().getContents().size(), 1 );
-        Assert.assertTrue( component.getHoverEvent().getContents().get( 0 ) instanceof HoverEvent.ContentText );
-        Assert.assertEquals( ( (HoverEvent.ContentText) component.getHoverEvent().getContents().get( 0 ) ).getValue(), advancement );
+        Assert.assertTrue( component.getHoverEvent().getContents().get( 0 ) instanceof Text );
+        Assert.assertEquals( ( (Text) component.getHoverEvent().getContents().get( 0 ) ).getValue(), advancement );
     }
 
     @Test
@@ -177,8 +194,8 @@ public class ComponentsTest
         // First do the text using the newer contents system
         HoverEvent hoverEvent = new HoverEvent(
                 HoverEvent.Action.SHOW_TEXT,
-                new HoverEvent.ContentText( new ComponentBuilder( "First" ).create() ),
-                new HoverEvent.ContentText( new ComponentBuilder( "Second" ).create() )
+                new Text( new ComponentBuilder( "First" ).create() ),
+                new Text( new ComponentBuilder( "Second" ).create() )
         );
 
         TextComponent component = new TextComponent( "Sample text" );
