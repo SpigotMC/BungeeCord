417475a32ffcaa136dbaaed05a7a4497894bd939
Vjatšeslav Maspanov <Vjatseslav.Maspanov@gmail.com>
2020-07-10 03:00:19 +0300

    Update version to dev

--- proxy/src/main/java/ru/leymooo/botfilter/config/Settings.java
+++ proxy/src/main/java/ru/leymooo/botfilter/config/Settings.java
@@ -20,7 +20,7 @@ public class Settings extends Config
     @Final
     public final String HELP = "http://www.rubukkit.org/threads/137038/";
     @Final
-    public String BOT_FILTER_VERSION = "3.8";
+    public String BOT_FILTER_VERSION = "3.8.1-dev";
 
     @Create
     public MESSAGES MESSAGES;
