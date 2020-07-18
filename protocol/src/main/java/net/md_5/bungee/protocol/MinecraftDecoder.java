15b514130eb861cb44ddc787aa152f73e259dbb1
Andrew Steinborn <git@steinborn.me>
2020-07-19 08:54:54 +1000

    #2908: Don't frame packets for dead connections

--- protocol/src/main/java/net/md_5/bungee/protocol/MinecraftDecoder.java
+++ protocol/src/main/java/net/md_5/bungee/protocol/MinecraftDecoder.java
@@ -20,6 +20,13 @@ public class MinecraftDecoder extends MessageToMessageDecoder<ByteBuf>
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception
     {
+        // See Varint21FrameDecoder for the general reasoning. We add this here as ByteToMessageDecoder#handlerRemoved()
+        // will fire any cumulated data through the pipeline, so we want to try and stop it here.
+        if ( !ctx.channel().isActive() )
+        {
+            return;
+        }
+
         Protocol.DirectionData prot = ( server ) ? protocol.TO_SERVER : protocol.TO_CLIENT;
         ByteBuf slice = in.copy(); // Can't slice this one due to EntityMap :(
 
