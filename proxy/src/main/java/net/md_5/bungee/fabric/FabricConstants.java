fd42c46c6827983d79a78161c48471ddbaad2e4f
Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
2020-12-28 19:32:30 +0100

    added experimental fabric support

---
+++ proxy/src/main/java/net/md_5/bungee/fabric/FabricConstants.java
@@ -0,0 +1,7 @@
+package net.md_5.bungee.fabric;
+
+public class FabricConstants
+{
+    public static final int FABRIC_API_PAYLOAD_ID = 0;
+    public static final String FABRIC_API_CHANNEL = "fabric-networking-api-v1:early_registration";
+}
