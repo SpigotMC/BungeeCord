SpigotMC/BungeeCord

commit 425dd4510989834c52d21d38e239ba0c7a4b02d2

Override classloader close method for completeness

api/src/main/java/net/md_5/bungee/api/plugin/PluginClassloader.java
@@ -54,16 +54,16 @@ final class PluginClassloader extends URLClassLoader
     }
 
     @Override
-    protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException
+    public Class<?> loadClass(String name) throws ClassNotFoundException
     {
-        return loadClass0( name, resolve, true, true );
+        return loadClass0( name, true, true );
     }
 
-    private Class<?> loadClass0(String name, boolean resolve, boolean checkOther, boolean checkLibraries) throws ClassNotFoundException
+    private Class<?> loadClass0(String name, boolean checkOther, boolean checkLibraries) throws ClassNotFoundException
     {
         try
         {
-            return super.loadClass( name, resolve );
+            return super.loadClass( name );
         } catch ( ClassNotFoundException ex )
         {
         }
@@ -86,7 +86,7 @@ final class PluginClassloader extends URLClassLoader
                 {
                     try
                     {
-                        return loader.loadClass0( name, resolve, false, proxy.getPluginManager().isTransitiveDepend( desc, loader.desc ) );
+                        return loader.loadClass0( name, false, proxy.getPluginManager().isTransitiveDepend( desc, loader.desc ) );
                     } catch ( ClassNotFoundException ex )
                     {
                     }
@@ -147,6 +147,18 @@ final class PluginClassloader extends URLClassLoader
         }
 
         throw new ClassNotFoundException( name );
+    }
+
+    @Override
+    public void close() throws IOException
+    {
+        try
+        {
+            super.close();
+        } finally
+        {
+            jar.close();
+        }
     }
 
     void init(Plugin plugin)

commit 72b3bdf6765bb703d4481b65e12d056d43abc9e0

#3077: Fix regression preventing child classloaders delegating to a PluginClassloader
Bug was introduced in 425dd4510989834c52d21d38e239ba0c7a4b02d2

api/src/main/java/net/md_5/bungee/api/plugin/PluginClassloader.java
@@ -54,16 +54,16 @@ final class PluginClassloader extends URLClassLoader
     }
 
     @Override
-    public Class<?> loadClass(String name) throws ClassNotFoundException
+    protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException
     {
-        return loadClass0( name, true, true );
+        return loadClass0( name, resolve, true, true );
     }
 
-    private Class<?> loadClass0(String name, boolean checkOther, boolean checkLibraries) throws ClassNotFoundException
+    private Class<?> loadClass0(String name, boolean resolve, boolean checkOther, boolean checkLibraries) throws ClassNotFoundException
     {
         try
         {
-            return super.loadClass( name );
+            return super.loadClass( name, resolve );
         } catch ( ClassNotFoundException ex )
         {
         }
@@ -86,7 +86,7 @@ final class PluginClassloader extends URLClassLoader
                 {
                     try
                     {
-                        return loader.loadClass0( name, false, proxy.getPluginManager().isTransitiveDepend( desc, loader.desc ) );
+                        return loader.loadClass0( name, resolve, false, proxy.getPluginManager().isTransitiveDepend( desc, loader.desc ) );
                     } catch ( ClassNotFoundException ex )
                     {
                     }