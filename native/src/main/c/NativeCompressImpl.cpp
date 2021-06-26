1b489bcc11e83631cd62f41154573120a2c104fe
md_5 <git@md-5.net>
2021-06-26 11:39:53 +1000

    Attempt to fix java 8 native crash

--- native/src/main/c/NativeCompressImpl.cpp
+++ native/src/main/c/NativeCompressImpl.cpp
@@ -6,7 +6,7 @@
 
 // Support for CentOS 6
 __asm__(".symver memcpy,memcpy@GLIBC_2.2.5");
-void *__wrap_memcpy(void *dest, const void *src, size_t n) {
+extern "C" void *__wrap_memcpy(void *dest, const void *src, size_t n) {
     return memcpy(dest, src, n);
 }
 
