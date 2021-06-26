da27924a49dad2a168eaa2cea49f1e751741d5fa
md_5 <git@md-5.net>
2021-06-26 11:28:42 +1000

    #3115, #3125: Update natives build script, switch to Cloudflare zlib

--- native/compile-native.sh
+++ native/compile-native.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-CXX="g++ -shared -fPIC -O3 -Wall -Werror -I$JAVA_HOME/include/ -I$JAVA_HOME/include/linux/"
+set -eu
 
-$CXX src/main/c/NativeCipherImpl.cpp -o src/main/resources/native-cipher.so -lcrypto
-$CXX src/main/c/NativeCompressImpl.cpp -o src/main/resources/native-compress.so -lz
+echo "Compiling mbedtls"
+(cd mbedtls && make no_test)
+
+echo "Compiling zlib"
+(cd zlib && CFLAGS=-fPIC ./configure --static && make)
+
+CXX="g++ -shared -fPIC -Wl,--wrap=memcpy -O3 -Wall -Werror -I$JAVA_HOME/include/ -I$JAVA_HOME/include/linux/"
+
+$CXX -Imbedtls/include src/main/c/NativeCipherImpl.cpp -o src/main/resources/native-cipher.so mbedtls/library/libmbedcrypto.a
+$CXX -Izlib src/main/c/NativeCompressImpl.cpp -o src/main/resources/native-compress.so zlib/libz.a
