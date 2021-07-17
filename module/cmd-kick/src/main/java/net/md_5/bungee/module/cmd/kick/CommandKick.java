e8a46e21f10f908195eb9e4bfe8761eba99b0113
FlorianMichael <jessica.reuth49@gmail.com>
2021-07-17 21:53:01 +0200

    added tab completion for kick command

--- module/cmd-kick/src/main/java/net/md_5/bungee/module/cmd/kick/CommandKick.java
+++ module/cmd-kick/src/main/java/net/md_5/bungee/module/cmd/kick/CommandKick.java
@@ -1,14 +1,20 @@
 package net.md_5.bungee.module.cmd.kick;
 
 import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableSet;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.api.plugin.TabExecutor;
 
-public class CommandKick extends Command
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+
+public class CommandKick extends Command implements TabExecutor
 {
 
     public CommandKick()
@@ -41,6 +47,27 @@ public class CommandKick extends Command
                 System.arraycopy( args, 1, reason, 0, reason.length );
                 player.disconnect( TextComponent.fromLegacyText( ChatColor.translateAlternateColorCodes( '&', Joiner.on( ' ' ).join( reason ) ) ) );
             }
+        }
+    }
+
+    @Override
+    public Iterable<String> onTabComplete(CommandSender sender, String[] args)
+    {
+        if ( args.length == 1 )
+        {
+            Set<String> matches = new HashSet<>();
+            String search = args[0].toLowerCase( Locale.ROOT );
+            for ( ProxiedPlayer player : ProxyServer.getInstance().getPlayers() )
+            {
+                if ( player.getName().toLowerCase( Locale.ROOT ).startsWith( search ) )
+                {
+                    matches.add( player.getName() );
+                }
+            }
+            return matches;
+        } else
+        {
+            return ImmutableSet.of();
         }
     }
 }
