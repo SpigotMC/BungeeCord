494c44d1fcd4a0ba8acc5b52773fc2552f277279
FlorianMichael <jessica.reuth49@gmail.com>
2021-07-17 21:04:58 +0200

    added gkick module with command

---
+++ module/cmd-kick/src/main/java/net/md_5/bungee/module/cmd/kick/CommandKick.java
@@ -0,0 +1,46 @@
+package net.md_5.bungee.module.cmd.kick;
+
+import com.google.common.base.Joiner;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.plugin.Command;
+
+public class CommandKick extends Command
+{
+
+    public CommandKick()
+    {
+        super("gkick", "bungeecord.command.kick");
+    }
+
+    @Override
+    public void execute(CommandSender sender, String[] args)
+    {
+        if ( args.length == 0 )
+        {
+            sender.sendMessage( ProxyServer.getInstance().getTranslation("username_needed") );
+        } else
+        {
+            ProxiedPlayer player = ProxyServer.getInstance().getPlayer( args[0] );
+
+            if ( player == null )
+            {
+                sender.sendMessage( ProxyServer.getInstance().getTranslation("user_not_online") );
+                return;
+            }
+
+            if ( args.length == 1 )
+            {
+                player.disconnect( TextComponent.fromLegacyText( ProxyServer.getInstance().getTranslation( "kick_message") ) );
+            } else
+            {
+                String[] reason = new String[ args.length - 1 ];
+                System.arraycopy( args, 1, reason, 0, reason.length );
+                player.disconnect( TextComponent.fromLegacyText( ChatColor.translateAlternateColorCodes( '&', Joiner.on( ' ' ).join( reason ) ) ) );
+            }
+        }
+    }
+}
