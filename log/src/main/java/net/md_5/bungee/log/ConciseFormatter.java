fd37fbb83b72abad845a3cfc6a452692fcf12fcd
Outfluencer <48880402+Outfluencer@users.noreply.github.com>
2021-12-20 14:51:37 +0100

    Add pretty color to PluginLogger
    Makes it look very nice

--- log/src/main/java/net/md_5/bungee/log/ConciseFormatter.java
+++ log/src/main/java/net/md_5/bungee/log/ConciseFormatter.java
@@ -27,6 +27,20 @@ public class ConciseFormatter extends Formatter
         formatted.append( " [" );
         appendLevel( formatted, record.getLevel() );
         formatted.append( "] " );
+
+        if ( record.getLoggerName() != null && !record.getLoggerName().equals( "BungeeCord" ) )
+        {
+            formatted.append( "[" );
+            if ( coloured )
+            {
+                formatted.append( ChatColor.GREEN ).append( record.getLoggerName() ).append( ChatColor.RESET );
+            } else
+            {
+                formatted.append( record.getLoggerName() );
+            }
+            formatted.append( "] " );
+        }
+
         formatted.append( formatMessage( record ) );
         formatted.append( '\n' );
 
