958a36f909d45f2d83824d3796966720526ff1f9
Outfluencer <48880402+Outfluencer@users.noreply.github.com>
2021-12-23 23:03:04 +0100

    don't hardcode BungeeCord

--- log/src/main/java/net/md_5/bungee/log/ConciseFormatter.java
+++ log/src/main/java/net/md_5/bungee/log/ConciseFormatter.java
@@ -16,6 +16,7 @@ public class ConciseFormatter extends Formatter
 
     private final DateFormat date = new SimpleDateFormat( System.getProperty( "net.md_5.bungee.log-date-format", "HH:mm:ss" ) );
     private final boolean coloured;
+    private final String loggerName;
 
     @Override
     @SuppressWarnings("ThrowableResultIgnored")
@@ -28,7 +29,7 @@ public class ConciseFormatter extends Formatter
         appendLevel( formatted, record.getLevel() );
         formatted.append( "] " );
 
-        if ( record.getLoggerName() != null && !record.getLoggerName().equals( "BungeeCord" ) )
+        if ( record.getLoggerName() != null && !record.getLoggerName().equals( loggerName ) )
         {
             formatted.append( "[" );
             if ( coloured )
