9a19a038b21f87e8c5f25ee65f99499a1700ac1f
Outfluencer <48880402+Outfluencer@users.noreply.github.com>
2022-02-09 19:27:05 +0100

    Update PlayerCommand.java

--- api/src/main/java/net/md_5/bungee/command/PlayerCommand.java
+++ api/src/main/java/net/md_5/bungee/command/PlayerCommand.java
@@ -29,7 +29,7 @@ public abstract class PlayerCommand extends Command implements TabExecutor
     {
         final String lastArg = ( args.length > 0 ) ? args[args.length - 1].toLowerCase( Locale.ROOT ) : "";
         return ProxyServer.getInstance().getPlayers().stream()
-                .map( (player) -> player.getName() )
+                .map( ProxiedPlayer::getName )
                 .filter( (name) -> name.toLowerCase( Locale.ROOT ).startsWith( lastArg ) )
                 .collect( Collectors.toList() );
     }
