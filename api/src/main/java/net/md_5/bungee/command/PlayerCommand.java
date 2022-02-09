99fc9fb5d8e0e5360b183878d859854aa2ef5a1f
Outfluencer <48880402+Outfluencer@users.noreply.github.com>
2022-02-09 20:30:36 +0100

    Update PlayerCommand.java

--- api/src/main/java/net/md_5/bungee/command/PlayerCommand.java
+++ api/src/main/java/net/md_5/bungee/command/PlayerCommand.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 import java.util.stream.Collectors;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.TabExecutor;
 
