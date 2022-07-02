a7476841a3795a3936b530ad9819c8b89a664e58
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-07-02 15:09:58 +0200

    make code lazy

--- module/cmd-plugins/src/main/java/de/florianmichael/bungee/module/cmd/plugins/CommandPlugins.java
+++ module/cmd-plugins/src/main/java/de/florianmichael/bungee/module/cmd/plugins/CommandPlugins.java
@@ -4,11 +4,10 @@ import com.google.common.collect.ImmutableSet;
 import java.util.HashSet;
 import java.util.Locale;
 import java.util.Set;
-import java.util.stream.Collectors;
+
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.api.plugin.TabExecutor;
@@ -28,30 +27,27 @@ public class CommandPlugins extends Command implements TabExecutor
 
         if ( args.length == 0 )
         {
-            Iterable<String> pluginNames = proxy.getPluginManager().getPlugins().stream().map( input -> input.getDescription().getName() ).collect( Collectors.toList() );
-
             StringBuilder pluginsList = new StringBuilder();
 
-            for ( String pluginName : pluginNames )
-            {
-                pluginsList.append( ChatColor.GREEN ).append( pluginName ).append( ChatColor.WHITE ).append( ", " );
-            }
+            proxy.getPluginManager().getPlugins().stream().map( input -> input.getDescription().getName() ).
+                    forEach( pluginName -> pluginsList.append( ChatColor.GREEN ).append( pluginName ).append( ChatColor.WHITE ).append( ", " ) );
+
             String plugins = pluginsList.substring( 0, pluginsList.lastIndexOf( "," ) - 1 );
             String translation = proxy.getTranslation( "command_plugins", proxy.getPluginManager().getPlugins().size(), plugins );
 
-            sender.sendMessage( TextComponent.fromLegacyText( translation ) );
+            sender.sendMessage( translation );
         } else
         {
             Plugin plugin = proxy.getPluginManager().getPlugin( args[0] );
 
             if ( plugin == null )
             {
-                sender.sendMessage( TextComponent.fromLegacyText( ProxyServer.getInstance().getTranslation( "plugin_null" ) ) );
+                sender.sendMessage( ProxyServer.getInstance().getTranslation( "plugin_null" ) );
             } else
             {
-                sender.sendMessage( TextComponent.fromLegacyText( ProxyServer.getInstance().getTranslation( "plugin_success",
-                        plugin.getDescription().getName(), plugin.getDescription().getVersion(), plugin.getDescription().getAuthor() ) ) );
-                sender.sendMessage( TextComponent.fromLegacyText( plugin.getDescription().getDescription() ) );
+                sender.sendMessage( ProxyServer.getInstance().getTranslation( "plugin_success",
+                        plugin.getDescription().getName(), plugin.getDescription().getVersion(), plugin.getDescription().getAuthor() ) );
+                sender.sendMessage( plugin.getDescription().getDescription() );
             }
         }
     }
