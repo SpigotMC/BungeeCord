e6a383ac5bd2f4f1700d8adcafc97538837ea1e4
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-07-03 14:34:31 +0200

    merged colors

--- module/cmd-plugins/src/main/java/net/md_5/bungee/module/cmd/plugins/CommandPlugins.java
+++ module/cmd-plugins/src/main/java/net/md_5/bungee/module/cmd/plugins/CommandPlugins.java
@@ -1,6 +1,7 @@
 package net.md_5.bungee.module.cmd.plugins;
 
 import com.google.common.collect.ImmutableSet;
+import java.io.File;
 import java.util.HashSet;
 import java.util.Locale;
 import java.util.Set;
@@ -26,11 +27,13 @@ public class CommandPlugins extends Command implements TabExecutor
 
         if ( args.length == 0 )
         {
+            File moduleFolder = new File( "modules" );
             StringBuilder pluginsList = new StringBuilder();
 
             for ( Plugin plugin : proxy.getPluginManager().getPlugins() )
             {
-                pluginsList.append( ChatColor.GREEN ).append( plugin.getDescription().getName() ).append( ChatColor.WHITE ).append( ", " );
+                boolean isModule = plugin.getFile().getParentFile().equals( moduleFolder );
+                pluginsList.append( isModule ? ChatColor.BLUE : ChatColor.GREEN ).append( plugin.getDescription().getName() ).append( ChatColor.WHITE ).append( ", " );
             }
 
             String plugins = pluginsList.substring( 0, pluginsList.lastIndexOf( "," ) - 1 );
