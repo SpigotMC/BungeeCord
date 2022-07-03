87e928fe91b9eba5ca991cf4274e01e148531047
Outfluencer <48880402+Outfluencer@users.noreply.github.com>
2022-07-03 22:27:00 +0200

    added import

--- module/cmd-kick/src/main/java/net/md_5/bungee/module/cmd/kick/CommandKick.java
+++ module/cmd-kick/src/main/java/net/md_5/bungee/module/cmd/kick/CommandKick.java
@@ -7,6 +7,7 @@ import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.command.PlayerCommand;
 
 public class CommandKick extends PlayerCommand
 {
