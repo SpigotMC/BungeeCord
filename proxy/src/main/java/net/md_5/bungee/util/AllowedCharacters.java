75dd74a891e21c35d0a4afdbd6d20940318f4c8c
Outfluencer <48880402+Outfluencer@users.noreply.github.com>
2022-08-17 11:39:35 +0200

    Allow extended ASCII chars for Premium Usernames
    related to https://github.com/SpigotMC/BungeeCord/issues/3379

--- proxy/src/main/java/net/md_5/bungee/util/AllowedCharacters.java
+++ proxy/src/main/java/net/md_5/bungee/util/AllowedCharacters.java
@@ -17,7 +17,7 @@ public final class AllowedCharacters
     {
         if ( onlineMode )
         {
-            return ( c >= 'a' && c <= 'z' ) || ( c >= '0' && c <= '9' ) || ( c >= 'A' && c <= 'Z' ) || c == '_' || c == '.' || c == '-';
+            return c > ' ' && c <= 255 && c != '\u00A7' && c != 127;
         } else
         {
             // Don't allow spaces, Yaml config doesn't support them
