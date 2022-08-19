764ef6c3905fe95c20f4720cd694dca180dde22e
rgnter <32541639+rgnter@users.noreply.github.com>
2022-08-19 17:36:34 +0200

    Update documentation of ProxyPingEvent.
    Tackling issue #3383

--- api/src/main/java/net/md_5/bungee/api/event/ProxyPingEvent.java
+++ api/src/main/java/net/md_5/bungee/api/event/ProxyPingEvent.java
@@ -8,7 +8,7 @@ import net.md_5.bungee.api.ServerPing;
 import net.md_5.bungee.api.connection.PendingConnection;
 
 /**
- * Called when the proxy is pinged with packet 0xFE from the server list.
+ * Called when the proxy is pinged with legacy server list ping packet ( 0xFE ), or status packet ( 0x00 ).
  */
 @Data
 @ToString(callSuper = false)
