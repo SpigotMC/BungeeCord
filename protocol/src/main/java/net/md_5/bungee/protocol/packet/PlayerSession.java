794f0632d1d7195ef84cbd882ec0a690f560a2a4
Outfluencer <48880402+Outfluencer@users.noreply.github.com>
2023-12-22 23:07:25 +0100

    add PlayerSession packet

---
+++ protocol/src/main/java/net/md_5/bungee/protocol/packet/PlayerSession.java
@@ -0,0 +1,41 @@
+package net.md_5.bungee.protocol.packet;
+
+import io.netty.buffer.ByteBuf;
+import java.util.UUID;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import net.md_5.bungee.protocol.AbstractPacketHandler;
+import net.md_5.bungee.protocol.DefinedPacket;
+import net.md_5.bungee.protocol.PlayerPublicKey;
+import net.md_5.bungee.protocol.ProtocolConstants;
+
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class PlayerSession extends DefinedPacket
+{
+
+    private UUID sessionId;
+    private PlayerPublicKey publicKey;
+
+    @Override
+    public void read(ByteBuf buf, ProtocolConstants.Direction direction, int protocolVersion)
+    {
+        sessionId = readUUID( buf );
+        publicKey = new PlayerPublicKey( buf.readLong(), readArray( buf, 512 ), readArray( buf, 4096 ) );
+    }
+
+    @Override
+    public void write(ByteBuf buf, ProtocolConstants.Direction direction, int protocolVersion)
+    {
+        writeUUID( sessionId, buf );
+        writePublicKey( publicKey, buf );
+    }
+
+    @Override
+    public void handle(AbstractPacketHandler handler) throws Exception
+    {
+        handler.handle( this );
+    }
+}
