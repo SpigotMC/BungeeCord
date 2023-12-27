f3d995cce7288a8504e1693718beb664f2a54ecd
Parker Hawke <hawkeboyz2@hotmail.com>
2023-12-27 15:28:07 -0500

    Remove unused imports as a result of previous merge

--- chat/src/main/java/net/md_5/bungee/chat/BaseComponentSerializer.java
+++ chat/src/main/java/net/md_5/bungee/chat/BaseComponentSerializer.java
@@ -4,8 +4,6 @@ import com.google.common.base.Preconditions;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
-import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import java.util.ArrayList;
 import java.util.Arrays;
