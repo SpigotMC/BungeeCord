dd7662a4a7d5195f7946df9d9a172618ee18a245
Janmm14 <gitconfig1@janmm14.de>
2024-04-21 13:36:19 +0200

    Chat tests: Use static imports for ChatColor, replace literal legacy color codes with ChatColor

--- chat/src/test/java/net/md_5/bungee/api/chat/TranslatableComponentTest.java
+++ chat/src/test/java/net/md_5/bungee/api/chat/TranslatableComponentTest.java
@@ -1,7 +1,7 @@
 package net.md_5.bungee.api.chat;
 
+import static net.md_5.bungee.api.ChatColor.*;
 import static org.junit.jupiter.api.Assertions.*;
-import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.chat.ComponentSerializer;
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +13,7 @@ public class TranslatableComponentTest
     {
         TranslatableComponent testComponent = new TranslatableComponent( "Test string with %s placeholders: %s", 2, "aoeu" );
         assertEquals( "Test string with 2 placeholders: aoeu", testComponent.toPlainText() );
-        assertEquals( "§fTest string with §f2§f placeholders: §faoeu", testComponent.toLegacyText() );
+        assertEquals( WHITE + "Test string with " + WHITE + "2" + WHITE + " placeholders: " + WHITE + "aoeu", testComponent.toLegacyText() );
     }
 
     @Test
@@ -24,26 +24,25 @@ public class TranslatableComponentTest
         BaseComponent[] baseComponents = ComponentSerializer.parse( jsonString );
 
         assertEquals( "Test string with a placeholder", BaseComponent.toPlainText( baseComponents ) );
-        assertEquals( "§fTest string with §fa§f placeholder", BaseComponent.toLegacyText( baseComponents ) );
+        assertEquals( WHITE + "Test string with " + WHITE + "a" + WHITE + " placeholder", BaseComponent.toLegacyText( baseComponents ) );
     }
 
     @Test
     public void testTranslateComponent()
     {
         TranslatableComponent item = new TranslatableComponent( "item.swordGold.name" );
-        item.setColor( ChatColor.AQUA );
+        item.setColor( AQUA );
         TranslatableComponent translatableComponent = new TranslatableComponent( "commands.give.success",
                 item, "5", "thinkofdeath" );
 
         assertEquals( "Given Golden Sword * 5 to thinkofdeath", translatableComponent.toPlainText() );
-        assertEquals( ChatColor.WHITE + "Given " + ChatColor.AQUA + "Golden Sword" + ChatColor.WHITE
-                        + " * " + ChatColor.WHITE + "5" + ChatColor.WHITE + " to " + ChatColor.WHITE + "thinkofdeath",
+        assertEquals( WHITE + "Given " + AQUA + "Golden Sword" + WHITE + " * " + WHITE + "5" + WHITE + " to " + WHITE + "thinkofdeath",
                 translatableComponent.toLegacyText() );
 
         TranslatableComponent positional = new TranslatableComponent( "book.pageIndicator", "5", "50" );
 
         assertEquals( "Page 5 of 50", positional.toPlainText() );
-        assertEquals( ChatColor.WHITE + "Page " + ChatColor.WHITE + "5" + ChatColor.WHITE + " of " + ChatColor.WHITE + "50", positional.toLegacyText() );
+        assertEquals( WHITE + "Page " + WHITE + "5" + WHITE + " of " + WHITE + "50", positional.toLegacyText() );
 
         TranslatableComponent one_four_two = new TranslatableComponent( "filled_map.buried_treasure" );
         assertEquals( "Buried Treasure Map", one_four_two.toPlainText() );
