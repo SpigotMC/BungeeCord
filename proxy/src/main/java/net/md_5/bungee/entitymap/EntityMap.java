5a9b1e44f72394cd2a2dd7a4d1f90b1591f3ce11
Outfluencer <realoutfluencer@gmail.com>
2024-08-22 13:16:43 +0200

    use right spectate id in entity rewritter

--- proxy/src/main/java/net/md_5/bungee/entitymap/EntityMap.java
+++ proxy/src/main/java/net/md_5/bungee/entitymap/EntityMap.java
@@ -88,8 +88,9 @@ public abstract class EntityMap
                 return EntityMap_1_16_2.INSTANCE_1_20_3;
             case ProtocolConstants.MINECRAFT_1_20_5:
             case ProtocolConstants.MINECRAFT_1_21:
-            case ProtocolConstants.MINECRAFT_1_21_2:
                 return EntityMap_1_16_2.INSTANCE_1_20_5;
+            case ProtocolConstants.MINECRAFT_1_21_2:
+                return EntityMap_1_16_2.INSTANCE_1_21_2;
         }
         throw new RuntimeException( "Version " + version + " has no entity map" );
     }
