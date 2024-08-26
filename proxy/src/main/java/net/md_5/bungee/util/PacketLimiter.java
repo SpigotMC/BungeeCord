4988053da6350f2d56e44736682944d76a31cf23
Outfluencer <48880402+Outfluencer@users.noreply.github.com>
2024-08-27 00:59:59 +0200

    no need for the getter anymore

--- proxy/src/main/java/net/md_5/bungee/util/PacketLimiter.java
+++ proxy/src/main/java/net/md_5/bungee/util/PacketLimiter.java
@@ -1,13 +1,11 @@
 package net.md_5.bungee.util;
 
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class PacketLimiter
 {
     // max amount of packets allowed per second
-    @Getter
     private final int limit;
     private int counter;
     private long nextSecond;
