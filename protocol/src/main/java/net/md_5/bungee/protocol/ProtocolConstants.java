fa0ca3da32b52ee794d5161d71bb7cdb3efb24a2
Outfluencer <git@outfluencer.dev>
2024-09-19 16:38:30 +0200

    Change protocol version

--- protocol/src/main/java/net/md_5/bungee/protocol/ProtocolConstants.java
+++ protocol/src/main/java/net/md_5/bungee/protocol/ProtocolConstants.java
@@ -47,7 +47,7 @@ public class ProtocolConstants
     public static final int MINECRAFT_1_20_3 = 765;
     public static final int MINECRAFT_1_20_5 = 766;
     public static final int MINECRAFT_1_21 = 767;
-    public static final int MINECRAFT_1_21_2 = 1073742032;
+    public static final int MINECRAFT_1_21_2 = 1073742034;
     public static final List<String> SUPPORTED_VERSIONS;
     public static final List<Integer> SUPPORTED_VERSION_IDS;
 
