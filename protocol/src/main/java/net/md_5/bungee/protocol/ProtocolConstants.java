20a71b06a9ef09544b8f794418cdae006d47b3d8
md_5 <git@md-5.net>
2024-11-30 10:11:33 +1100

    Minecraft 1.21.4-rc3 support

--- protocol/src/main/java/net/md_5/bungee/protocol/ProtocolConstants.java
+++ protocol/src/main/java/net/md_5/bungee/protocol/ProtocolConstants.java
@@ -48,7 +48,7 @@ public class ProtocolConstants
     public static final int MINECRAFT_1_20_5 = 766;
     public static final int MINECRAFT_1_21 = 767;
     public static final int MINECRAFT_1_21_2 = 768;
-    public static final int MINECRAFT_1_21_4 = 1073742048;
+    public static final int MINECRAFT_1_21_4 = 1073742052;
     public static final List<String> SUPPORTED_VERSIONS;
     public static final List<Integer> SUPPORTED_VERSION_IDS;
 
