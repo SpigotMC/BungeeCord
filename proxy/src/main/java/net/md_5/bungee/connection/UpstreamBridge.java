0aa2871b261681dc53c71ae3ed8f177ce97fe6e1
Outfluencer <git@outfluencer.dev>
2025-01-22 21:11:11 +0100

    #3761: Whitelist LoginPayloadResponse in UpstreamBridge#shouldHandle
    Required for #3758 to function correctly.

--- proxy/src/main/java/net/md_5/bungee/connection/UpstreamBridge.java
+++ proxy/src/main/java/net/md_5/bungee/connection/UpstreamBridge.java
@@ -132,7 +132,7 @@ public class UpstreamBridge extends PacketHandler
     @Override
     public boolean shouldHandle(PacketWrapper packet) throws Exception
     {
-        return con.getServer() != null || packet.packet instanceof PluginMessage || packet.packet instanceof CookieResponse;
+        return con.getServer() != null || packet.packet instanceof PluginMessage || packet.packet instanceof CookieResponse || packet.packet instanceof LoginPayloadResponse;
     }
 
     @Override
