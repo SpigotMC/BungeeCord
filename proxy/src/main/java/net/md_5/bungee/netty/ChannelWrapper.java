b0ac7b92bf3bbc5150f0ac9f23a7e7f369d1c3e6
Outfluencer <git@outfluencer.dev>
2025-02-16 17:57:03 +0100

    Disable auto read on close
    otherwise the pipe will still process packets untill netty inactivates the channel

--- proxy/src/main/java/net/md_5/bungee/netty/ChannelWrapper.java
+++ proxy/src/main/java/net/md_5/bungee/netty/ChannelWrapper.java
@@ -132,6 +132,9 @@ public class ChannelWrapper
         {
             closed = closing = true;
 
+            // disable auto read so the pipeline doesn't read more traffic
+            ch.config().setAutoRead( false );
+
             if ( packet != null && ch.isActive() )
             {
                 ch.writeAndFlush( packet ).addListeners( ChannelFutureListener.FIRE_EXCEPTION_ON_FAILURE, ChannelFutureListener.CLOSE );
@@ -148,6 +151,9 @@ public class ChannelWrapper
         if ( !closing )
         {
             closing = true;
+
+            // disable auto read so the pipeline doesn't read more traffic
+            ch.config().setAutoRead( false );
 
             // Minecraft client can take some time to switch protocols.
             // Sending the wrong disconnect packet whilst a protocol switch is in progress will crash it.
