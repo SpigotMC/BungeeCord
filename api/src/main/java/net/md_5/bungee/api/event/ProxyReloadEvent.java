6cb5c0cc8d64f513b68418864ede62b620c79b7b
Outfluencer <git@outfluencer.dev>
2025-02-17 19:23:31 +0100

    Impl toString for ProxyReloadEvent
    Its the only event that doesn't have it

--- api/src/main/java/net/md_5/bungee/api/event/ProxyReloadEvent.java
+++ api/src/main/java/net/md_5/bungee/api/event/ProxyReloadEvent.java
@@ -3,6 +3,7 @@ package net.md_5.bungee.api.event;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.plugin.Event;
 
@@ -10,6 +11,7 @@ import net.md_5.bungee.api.plugin.Event;
  * Called when somebody reloads BungeeCord
  */
 @Getter
+@ToString(callSuper = false)
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = false)
 public class ProxyReloadEvent extends Event
