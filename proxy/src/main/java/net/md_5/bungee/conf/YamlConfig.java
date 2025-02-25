b4d6dca7262469d1ecd427381f673d72a955b3f3
Juan Cruz Linsalata <25271111+linsaftw@users.noreply.github.com>
2025-02-25 11:27:05 -0300

    Force wrong configuration values
    Pterodactyl and other type of panels force outdated values into BungeeCord. When BungeeCord changes the type of an already existing value, panels keep forcing old values causing exceptions on BungeeCord load and leading to a crash.
    
    This patch aims to convert the wrong values into the right values.

--- proxy/src/main/java/net/md_5/bungee/conf/YamlConfig.java
+++ proxy/src/main/java/net/md_5/bungee/conf/YamlConfig.java
@@ -111,32 +111,36 @@ public class YamlConfig implements ConfigurationAdapter
         return get( path, def, config );
     }
 
-    @SuppressWarnings("unchecked")
-    private <T> T get(String path, T def, Map submap)
-    {
-        int index = path.indexOf( '.' );
-        if ( index == -1 )
-        {
-            Object val = submap.get( path );
-            if ( val == null && def != null )
-            {
-                val = def;
-                submap.put( path, def );
-                save();
-            }
-            return (T) val;
-        } else
-        {
-            String first = path.substring( 0, index );
-            String second = path.substring( index + 1, path.length() );
-            Map sub = (Map) submap.get( first );
-            if ( sub == null )
-            {
-                sub = new LinkedHashMap();
-                submap.put( first, sub );
-            }
-            return get( second, def, sub );
+    private <T> T convertValue(Object value, T defaultValue) {
+        if (value == null) {
+            return defaultValue;
         }
+    
+        if (defaultValue instanceof Integer) {
+            return (T) convertToInteger(value);
+        }
+    
+        if (defaultValue instanceof Double) {
+            return (T) convertToDouble(value);
+        }
+    
+        // Add more type conversions as needed
+    
+        return (T) value;
+    }
+    
+    private Integer convertToInteger(Object value) {
+        if (value instanceof Number) {
+            return ((Number) value).intValue();
+        }
+        return Integer.valueOf(value.toString());
+    }
+    
+    private Double convertToDouble(Object value) {
+        if (value instanceof Number) {
+            return ((Number) value).doubleValue();
+        }
+        return Double.valueOf(value.toString());
     }
 
     private void set(String path, Object val)
