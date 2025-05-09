2bafb70581f7b460abf96b877814a6dda0d0a2be
md_5 <git@md-5.net>
2025-05-09 19:45:28 +1000

    Minecraft 25w19a protocol support

--- protocol/src/main/java/net/md_5/bungee/protocol/ProtocolConstants.java
+++ protocol/src/main/java/net/md_5/bungee/protocol/ProtocolConstants.java
@@ -50,7 +50,7 @@ public class ProtocolConstants
     public static final int MINECRAFT_1_21_2 = 768;
     public static final int MINECRAFT_1_21_4 = 769;
     public static final int MINECRAFT_1_21_5 = 770;
-    public static final int MINECRAFT_1_21_6 = 1073742072;
+    public static final int MINECRAFT_1_21_6 = 1073742073;
     public static final List<String> SUPPORTED_VERSIONS;
     public static final List<Integer> SUPPORTED_VERSION_IDS;
 
