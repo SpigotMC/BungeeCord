ec521c38fa7393cc71348f1ca46d06f36ec0d2ed
Outfluencer <git@outfluencer.dev>
2025-05-20 19:56:35 +0200

    25w21a changes (multiline support)

--- dialog/src/main/java/net/md_5/bungee/api/dialog/input/TextInput.java
+++ dialog/src/main/java/net/md_5/bungee/api/dialog/input/TextInput.java
@@ -19,18 +19,44 @@ public class TextInput extends DialogInput
     @SerializedName("label_visible")
     private boolean labelVisible;
     private String initial;
+    @SerializedName("max_length")
+    private int maxLength;
+    /**
+     * if set, allows users to input multiple lines
+     */
+    private Multiline multiline;
 
     public TextInput(String key, BaseComponent label)
     {
-        this( key, 200, label, true, "" );
+        this( key, 200, label, true, null, 32, null );
     }
 
-    public TextInput(String key, int width, BaseComponent label, boolean labelVisible, String initial)
+    public TextInput(String key, int width, BaseComponent label, boolean labelVisible, String initial, Integer maxLength)
+    {
+        this( key, width, label, labelVisible, initial, maxLength, null );
+    }
+
+    public TextInput(String key, int width, BaseComponent label, boolean labelVisible, String initial, Integer maxLength, Multiline multiline)
     {
         super( "minecraft:text", key );
         this.width = width;
         this.label = label;
         this.labelVisible = labelVisible;
         this.initial = initial;
+        this.maxLength = maxLength;
+        this.multiline = multiline;
+    }
+
+    @Data
+    @Accessors(fluent = true)
+    @ToString(callSuper = true)
+    @EqualsAndHashCode(callSuper = false)
+    public static class Multiline {
+        /**
+         * The maximum length of input
+         */
+        @SerializedName("max_lines")
+        private Integer maxLines;
+        private Integer height = 32;
     }
 }
