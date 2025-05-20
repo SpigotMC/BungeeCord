68a893d4f35beaf3e8c5b1bcee1875b7520e2836
Outfluencer <git@outfluencer.dev>
2025-05-20 19:35:39 +0200

    Fix NumberRangeInput
    make the values that are Optional nullable

--- dialog/src/main/java/net/md_5/bungee/api/dialog/input/NumberRangeInput.java
+++ dialog/src/main/java/net/md_5/bungee/api/dialog/input/NumberRangeInput.java
@@ -18,15 +18,25 @@ public class NumberRangeInput extends DialogInput
     private String labelFormat;
     private float start;
     private float end;
-    private float step;
-    private float initial;
+    private Float step;
+    private Float initial;
 
-    public NumberRangeInput(String key, BaseComponent label, float start, float end, float step)
+    public NumberRangeInput(String key, BaseComponent label, float start, float end)
     {
-        this( key, 200, label, "options.generic_value", start, end, step, start );
+        this( key, 200, label, "options.generic_value", start, end, null, null );
     }
 
-    public NumberRangeInput(String key, int width, BaseComponent label, String labelFormat, float start, float end, float step, float initial)
+    public NumberRangeInput(String key, BaseComponent label, float start, float end, Float step)
+    {
+        this( key, 200, label, "options.generic_value", start, end, step, null );
+    }
+
+    public NumberRangeInput(String key, BaseComponent label, float start, float end, Float step, Float initial)
+    {
+        this( key, 200, label, "options.generic_value", start, end, step, initial );
+    }
+
+    public NumberRangeInput(String key, int width, BaseComponent label, String labelFormat, float start, float end, Float step, Float initial)
     {
         super( "minecraft:number_range", key );
         this.width = width;
