bas1csyntax/Skyrail

Change to provide deserializable implementations with inbuilt converters and split either-sourced and new-only-sourced.

protocol/src/main/java/net/md_5/bungee/protocol/DefinedPacket.java
@@ -26,7 +26,10 @@ import net.md_5.bungee.protocol.data.NumberFormat;
 import net.md_5.bungee.protocol.data.PlayerPublicKey;
 import net.md_5.bungee.protocol.data.Property;
 import net.md_5.bungee.protocol.util.ChatDeserializable;
-import net.md_5.bungee.protocol.util.ChatFunctionDeserializable;
+import net.md_5.bungee.protocol.util.ChatDeserializableJson;
+import net.md_5.bungee.protocol.util.ChatDeserializableTag;
+import net.md_5.bungee.protocol.util.ChatNewDeserializable;
+import net.md_5.bungee.protocol.util.ChatNewDeserializableTag;
 import net.md_5.bungee.protocol.util.Either;
 import net.md_5.bungee.protocol.util.TagUtil;
 import se.llbit.nbt.ErrorTag;
@@ -103,7 +106,7 @@ public abstract class DefinedPacket
 
     public static Either<String, ChatDeserializable> readEitherBaseComponent(ByteBuf buf, int protocolVersion, boolean string)
     {
-        return ( string ) ? Either.left( readString( buf ) ) : Either.right( readBaseComponent( buf, protocolVersion ) );
+        return string ? Either.left( readString( buf ) ) : Either.right( readBaseComponent( buf, protocolVersion ) );
     }
 
     public static ChatDeserializable readBaseComponent(ByteBuf buf, int protocolVersion)
@@ -115,14 +118,10 @@ public abstract class DefinedPacket
     {
         if ( protocolVersion >= ProtocolConstants.MINECRAFT_1_20_3 )
         {
-            SpecificTag nbt = (SpecificTag) readTag( buf, protocolVersion );
-
-            return new ChatFunctionDeserializable( Either.right( nbt ), (ov) -> ComponentSerializer.deserialize( TagUtil.toJson( ov.getRight() ) ) );
+            return new ChatDeserializableTag( (SpecificTag) readTag( buf, protocolVersion ) );
         } else
         {
-            String string = readString( buf, maxStringLength );
-
-            return new ChatFunctionDeserializable( Either.left( string ), (ov) -> ComponentSerializer.deserialize( ov.getLeft() ) );
+            return new ChatDeserializableJson( readString( buf, maxStringLength ) );
         }
     }
 
@@ -145,6 +144,13 @@ public abstract class DefinedPacket
 
             return ComponentSerializer.deserialize( string );
         }
+    }
+
+    public static ChatNewDeserializable readNewBaseComponent(ByteBuf buf, int protocolVersion)
+    {
+        SpecificTag nbt = (SpecificTag) readTag( buf, protocolVersion );
+
+        return new ChatNewDeserializableTag( nbt );
     }
 
     public static ComponentStyle readComponentStyle(ByteBuf buf, int protocolVersion)
@@ -225,6 +231,19 @@ public abstract class DefinedPacket
 
             writeString( string, buf );
         }
+    }
+
+    public static void writeNewBaseComponent(ChatNewDeserializable message, ByteBuf buf, int protocolVersion)
+    {
+        SpecificTag nbt;
+        if ( message.hasDeserialized() )
+        {
+            nbt = TagUtil.fromJson( ComponentSerializer.toJson( message.get() ) );
+        } else
+        {
+            nbt = message.original();
+        }
+        writeTag( nbt, buf, protocolVersion );
     }
 
     public static void writeComponentStyle(ComponentStyle style, ByteBuf buf, int protocolVersion)

protocol/src/main/java/net/md_5/bungee/protocol/util/ChatNewCapturingDeserializable.java
@@ -1,0 +1,49 @@
+package net.md_5.bungee.protocol.util;
+
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import net.md_5.bungee.api.chat.BaseComponent;
+import se.llbit.nbt.SpecificTag;
+
+@ToString
+@EqualsAndHashCode
+@RequiredArgsConstructor
+public abstract class ChatNewCapturingDeserializable implements ChatNewDeserializable
+{
+    @NonNull
+    private final SpecificTag original;
+    private BaseComponent deserialized;
+
+    /**
+     * Method called to get the deserialized value. Called only once unless multiple threads are calling get() at the
+     * same time.
+     *
+     * @return the deserialized value
+     */
+    @NonNull
+    protected abstract BaseComponent deserialize();
+
+    @Override
+    public final BaseComponent get()
+    {
+        if ( !hasDeserialized() )
+        {
+            return deserialized = deserialize();
+        }
+        return deserialized;
+    }
+
+    @Override
+    public final boolean hasDeserialized()
+    {
+        return deserialized != null;
+    }
+
+    @Override
+    public final SpecificTag original()
+    {
+        return original;
+    }
+}

protocol/src/main/java/net/md_5/bungee/protocol/util/ChatNewComponentDeserializable.java
@@ -1,0 +1,33 @@
+package net.md_5.bungee.protocol.util;
+
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import net.md_5.bungee.api.chat.BaseComponent;
+import se.llbit.nbt.SpecificTag;
+
+@ToString
+@EqualsAndHashCode
+@RequiredArgsConstructor
+public class ChatNewComponentDeserializable implements ChatNewDeserializable
+{
+    private final BaseComponent value;
+
+    @Override
+    public BaseComponent get()
+    {
+        return value;
+    }
+
+    @Override
+    public SpecificTag original()
+    {
+        return null;
+    }
+
+    @Override
+    public boolean hasDeserialized()
+    {
+        return true;
+    }
+}

SpigotMC/BungeeCord

protocol/src/main/java/net/md_5/bungee/protocol/DefinedPacket.java
@@ -31,7 +31,10 @@ import net.md_5.bungee.protocol.data.NumberFormat;
 import net.md_5.bungee.protocol.data.PlayerPublicKey;
 import net.md_5.bungee.protocol.data.Property;
 import net.md_5.bungee.protocol.util.ChatDeserializable;
-import net.md_5.bungee.protocol.util.ChatFunctionDeserializable;
+import net.md_5.bungee.protocol.util.ChatDeserializableJson;
+import net.md_5.bungee.protocol.util.ChatDeserializableTag;
+import net.md_5.bungee.protocol.util.ChatNewDeserializable;
+import net.md_5.bungee.protocol.util.ChatNewDeserializableTag;
 import net.md_5.bungee.protocol.util.Either;
 import net.md_5.bungee.protocol.util.TagUtil;
 
@@ -113,7 +116,7 @@ public abstract class DefinedPacket
 
     public static Either<String, ChatDeserializable> readEitherBaseComponent(ByteBuf buf, int protocolVersion, boolean string)
     {
-        return ( string ) ? Either.left( readString( buf ) ) : Either.right( readBaseComponent( buf, protocolVersion ) );
+        return string ? Either.left( readString( buf ) ) : Either.right( readBaseComponent( buf, protocolVersion ) );
     }
 
     public static ChatDeserializable readBaseComponent(ByteBuf buf, int protocolVersion)
@@ -125,14 +128,10 @@ public abstract class DefinedPacket
     {
         if ( protocolVersion >= ProtocolConstants.MINECRAFT_1_20_3 )
         {
-            TypedTag nbt = (TypedTag) readTag( buf, protocolVersion );
-
-            return new ChatFunctionDeserializable( Either.right( nbt ), (ov) -> ChatSerializer.forVersion( protocolVersion ).deserialize( TagUtil.toJson( ov.getRight() ) ) );
+            return new ChatDeserializableTag( protocolVersion, (TypedTag) readTag( buf, protocolVersion ) );
         } else
         {
-            String string = readString( buf, maxStringLength );
-
-            return new ChatFunctionDeserializable( Either.left( string ), (ov) -> ChatSerializer.forVersion( protocolVersion ).deserialize( ov.getLeft() ) );
+            return new ChatDeserializableJson( protocolVersion, readString( buf, maxStringLength ) );
         }
     }
 
@@ -155,6 +154,13 @@ public abstract class DefinedPacket
 
             return ChatSerializer.forVersion( protocolVersion ).deserialize( string );
         }
+    }
+
+    public static ChatNewDeserializable readNewBaseComponent(ByteBuf buf, int protocolVersion)
+    {
+        TypedTag nbt = (TypedTag) readTag( buf, protocolVersion );
+
+        return new ChatNewDeserializableTag( protocolVersion, nbt );
     }
 
     public static ComponentStyle readComponentStyle(ByteBuf buf, int protocolVersion)
@@ -234,6 +240,19 @@ public abstract class DefinedPacket
 
             writeString( string, buf );
         }
+    }
+
+    public static void writeNewBaseComponent(ChatNewDeserializable message, ByteBuf buf, int protocolVersion)
+    {
+        TypedTag nbt;
+        if ( message.hasDeserialized() )
+        {
+            nbt = TagUtil.fromJson( ChatSerializer.forVersion( protocolVersion ).toJson( message.get() ) );
+        } else
+        {
+            nbt = message.original();
+        }
+        writeTag( nbt, buf, protocolVersion );
     }
 
     public static void writeComponentStyle(ComponentStyle style, ByteBuf buf, int protocolVersion)

protocol/src/main/java/net/md_5/bungee/protocol/util/ChatNewCapturingDeserializable.java
@@ -1,0 +1,49 @@
+package net.md_5.bungee.protocol.util;
+
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.nbt.TypedTag;
+
+@ToString
+@EqualsAndHashCode
+@RequiredArgsConstructor
+public abstract class ChatNewCapturingDeserializable implements ChatNewDeserializable
+{
+    @NonNull
+    private final TypedTag original;
+    private BaseComponent deserialized;
+
+    /**
+     * Method called to get the deserialized value. Called only once unless multiple threads are calling get() at the
+     * same time.
+     *
+     * @return the deserialized value
+     */
+    @NonNull
+    protected abstract BaseComponent deserialize();
+
+    @Override
+    public final BaseComponent get()
+    {
+        if ( !hasDeserialized() )
+        {
+            return deserialized = deserialize();
+        }
+        return deserialized;
+    }
+
+    @Override
+    public final boolean hasDeserialized()
+    {
+        return deserialized != null;
+    }
+
+    @Override
+    public final TypedTag original()
+    {
+        return original;
+    }
+}

protocol/src/main/java/net/md_5/bungee/protocol/util/ChatNewComponentDeserializable.java
@@ -1,0 +1,33 @@
+package net.md_5.bungee.protocol.util;
+
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.nbt.TypedTag;
+
+@ToString
+@EqualsAndHashCode
+@RequiredArgsConstructor
+public class ChatNewComponentDeserializable implements ChatNewDeserializable
+{
+    private final BaseComponent value;
+
+    @Override
+    public BaseComponent get()
+    {
+        return value;
+    }
+
+    @Override
+    public TypedTag original()
+    {
+        return null;
+    }
+
+    @Override
+    public boolean hasDeserialized()
+    {
+        return true;
+    }
+}