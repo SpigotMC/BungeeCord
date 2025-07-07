7fcc62067b76a51a7c0f14d1d018ff11075a939e
2008Choco <hawkeboyz2@hotmail.com>
2025-07-07 20:48:37 +1000

    #3858: Replace pre-Java 8 functional interface instantiation in CommandServer

--- module/cmd-server/src/main/java/net/md_5/bungee/module/cmd/server/CommandServer.java
+++ module/cmd-server/src/main/java/net/md_5/bungee/module/cmd/server/CommandServer.java
@@ -1,9 +1,5 @@
 package net.md_5.bungee.module.cmd.server;
 
-import com.google.common.base.Function;
-import com.google.common.base.Predicate;
-import com.google.common.collect.Iterables;
-import java.util.Collections;
 import java.util.Locale;
 import java.util.Map;
 import net.md_5.bungee.api.CommandSender;
@@ -83,22 +79,10 @@ public class CommandServer extends Command implements TabExecutor
     @Override
     public Iterable<String> onTabComplete(final CommandSender sender, final String[] args)
     {
-        return ( args.length > 1 ) ? Collections.EMPTY_LIST : Iterables.transform( Iterables.filter( ProxyServer.getInstance().getServers().values(), new Predicate<ServerInfo>()
-        {
-            private final String lower = ( args.length == 0 ) ? "" : args[0].toLowerCase( Locale.ROOT );
-
-            @Override
-            public boolean apply(ServerInfo input)
-            {
-                return input.getName().toLowerCase( Locale.ROOT ).startsWith( lower ) && input.canAccess( sender );
-            }
-        } ), new Function<ServerInfo, String>()
-        {
-            @Override
-            public String apply(ServerInfo input)
-            {
-                return input.getName();
-            }
-        } );
+        final String serverFilter = ( args.length == 0 ) ? "" : args[0].toLowerCase( Locale.ROOT );
+        return () -> ProxyServer.getInstance().getServers().values().stream()
+                .filter( serverInfo -> serverInfo.getName().toLowerCase( Locale.ROOT ).startsWith( serverFilter ) && serverInfo.canAccess( sender ) )
+                .map( ServerInfo::getName )
+                .iterator();
     }
 }
