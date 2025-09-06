ce8c3951dc5b91f289bc3c7eaf7e99b8644a143c
Outfluencer <git@outfluencer.dev>
2025-09-06 17:25:58 +0200

    Add docs

--- api/src/main/java/net/md_5/bungee/api/event/PlayerConfigurationEvent.java
+++ api/src/main/java/net/md_5/bungee/api/event/PlayerConfigurationEvent.java
@@ -6,11 +6,17 @@ import lombok.ToString;
 import net.md_5.bungee.api.Callback;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
+/**
+ * Called when the player enters configuration phase.
+ */
 @Data
 @ToString(callSuper = false)
 @EqualsAndHashCode(callSuper = false)
 public class PlayerConfigurationEvent extends AsyncEvent<PlayerConfigurationEvent>
 {
+    /**
+     * The player entering configuration phase.
+     */
     private ProxiedPlayer player;
 
     public PlayerConfigurationEvent(ProxiedPlayer player, Callback<PlayerConfigurationEvent> done)
