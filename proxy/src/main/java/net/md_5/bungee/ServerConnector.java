95bdd972afde8134465648d8f3add714e15b00f9
Outfluencer <git@outfluencer.dev>
2025-11-10 17:56:37 +0100

    space

--- proxy/src/main/java/net/md_5/bungee/ServerConnector.java
+++ proxy/src/main/java/net/md_5/bungee/ServerConnector.java
@@ -221,7 +221,7 @@ public class ServerConnector extends PacketHandler
         ServerConnectedEvent event = new ServerConnectedEvent( user, server );
         bungee.getPluginManager().callEvent( event );
 
-         // Already sent after config start
+        // Already sent after config start
         if ( user.getPendingConnection().getVersion() < ProtocolConstants.MINECRAFT_1_20_2 )
         {
             ch.write( BungeeCord.getInstance().registerChannels( user.getPendingConnection().getVersion() ) );
