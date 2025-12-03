de925facfe904b7c8ba22f065841af9d55286b35
Outfluencer <git@outfluencer.dev>
2025-12-03 13:18:00 +0100

    Add benchmark

---
+++ event/src/test/java/net/md_5/bungee/event/bench/MethodHandleBenchmark.java
@@ -0,0 +1,76 @@
+package net.md_5.bungee.event.bench;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+
+@State(Scope.Thread)
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Fork(value = 1, warmups = 1)
+@Warmup(iterations = 1, time = 1)
+@Measurement(iterations = 1, time = 1)
+public class MethodHandleBenchmark
+{
+
+    public static class TestEvent
+    {
+    }
+
+    public static class TestListener
+    {
+        public void onEvent(TestEvent e)
+        {
+        }
+    }
+
+    // Variables we need
+    private TestEvent event;
+    private TestListener listener;
+    private Method reflectionMethod;
+    private MethodHandle methodHandle;
+
+    @Setup
+    public void setup() throws Throwable
+    {
+        event = new TestEvent();
+        listener = new TestListener();
+
+        // Setup Reflection
+        reflectionMethod = TestListener.class.getMethod( "onEvent", TestEvent.class );
+
+        // Setup MethodHandle
+        methodHandle = MethodHandles.lookup().unreflect( reflectionMethod ).bindTo( listener ).asType( MethodType.methodType( void.class, Object.class ) );
+    }
+
+    @Benchmark
+    public void testDirectCall(Blackhole bh)
+    {
+        listener.onEvent( event );
+    }
+
+    @Benchmark
+    public void testReflection(Blackhole bh) throws Exception
+    {
+        reflectionMethod.invoke( listener, event );
+    }
+
+    @Benchmark
+    public void testMethodHandle(Blackhole bh) throws Throwable
+    {
+        methodHandle.invokeExact( (Object) event );
+    }
+}
