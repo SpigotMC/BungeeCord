46371395219f4070f2ac3c47f997288d9fe524de
SpigotRCE <128710385+SpigotRCE@users.noreply.github.com>
2025-12-24 21:47:41 +0530

    use lombok non null annotation

--- api/src/main/java/net/md_5/bungee/api/plugin/TabExecutor.java
+++ api/src/main/java/net/md_5/bungee/api/plugin/TabExecutor.java
@@ -2,10 +2,10 @@ package net.md_5.bungee.api.plugin;
 
 import net.md_5.bungee.api.CommandSender;
 
-import org.jetbrains.annotations.NotNull;
+import lombok.NonNull;
 
 public interface TabExecutor
 {
 
-    public @NotNull Iterable<String> onTabComplete(CommandSender sender, String[] args);
+    public @NonNull Iterable<String> onTabComplete(CommandSender sender, String[] args);
 }
