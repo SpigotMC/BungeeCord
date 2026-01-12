9a9f158492121a1b47abf068594ee6ce216fd6bd
Outfluencer <git@outfluencer.dev>
2026-01-12 21:10:45 +0100

    Create FastByteToByteDecoder to reduce netty overhead

---
+++ protocol/src/main/java/net/md_5/bungee/protocol/FastByteToByteDecoder.java
@@ -0,0 +1,41 @@
+package net.md_5.bungee.protocol;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.DecoderException;
+import io.netty.util.ReferenceCountUtil;
+
+public abstract class FastByteToByteDecoder extends ChannelInboundHandlerAdapter
+{
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception
+    {
+        if ( msg instanceof ByteBuf )
+        {
+            ByteBuf out;
+            try
+            {
+                out = decode( ctx, (ByteBuf) msg );
+            } catch ( DecoderException e )
+            {
+                throw e;
+            } catch ( Exception e )
+            {
+                throw new DecoderException( e );
+            }
+            ReferenceCountUtil.release( msg );
+            if ( out != null )
+            {
+                ctx.fireChannelRead( out );
+            }
+        } else
+        {
+            ctx.fireChannelRead( msg );
+        }
+    }
+
+
+    protected abstract ByteBuf decode(ChannelHandlerContext ctx, ByteBuf in) throws Exception;
+}
--- proxy/src/main/java/net/md_5/bungee/netty/cipher/CipherDecoder.java
+++ proxy/src/main/java/net/md_5/bungee/netty/cipher/CipherDecoder.java
@@ -2,26 +2,25 @@ package net.md_5.bungee.netty.cipher;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.MessageToMessageDecoder;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import net.md_5.bungee.jni.cipher.BungeeCipher;
+import net.md_5.bungee.protocol.FastByteToByteDecoder;
 
 @RequiredArgsConstructor
-public class CipherDecoder extends MessageToMessageDecoder<ByteBuf>
+public class CipherDecoder extends FastByteToByteDecoder
 {
 
     private final BungeeCipher cipher;
 
     @Override
-    protected void decode(ChannelHandlerContext ctx, ByteBuf msg, List<Object> out) throws Exception
-    {
-        out.add( cipher.cipher( ctx, msg ) );
-    }
-
-    @Override
     public void handlerRemoved(ChannelHandlerContext ctx) throws Exception
     {
         cipher.free();
+    }
+
+    @Override
+    protected ByteBuf decode(ChannelHandlerContext ctx, ByteBuf in) throws Exception
+    {
+        return cipher.cipher( ctx, in );
     }
 }
