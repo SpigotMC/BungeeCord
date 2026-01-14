9603dcd3028b2729c8e30e94287df0d68df138da
benediktjohannes <benedikt.johannes.hofer@gmail.com>
2026-01-14 21:05:28 +0100

    Update CommandBungee.java
    Added missing "."

--- proxy/src/main/java/net/md_5/bungee/command/CommandBungee.java
+++ proxy/src/main/java/net/md_5/bungee/command/CommandBungee.java
@@ -16,6 +16,6 @@ public class CommandBungee extends Command
     @Override
     public void execute(CommandSender sender, String[] args)
     {
-        sender.sendMessage( ChatColor.BLUE + "This server is running BungeeCord version " + ProxyServer.getInstance().getVersion() + " by md_5" );
+        sender.sendMessage( ChatColor.BLUE + "This server is running BungeeCord version " + ProxyServer.getInstance().getVersion() + " by md_5." );
     }
 }
