d269b56f0501b29074e27478e8af54325ac00124
md_5 <git@md-5.net>
2026-03-14 14:26:04 +1100

    Update mbedtls/zlib

--- native/compile-native-arm.sh
+++ native/compile-native-arm.sh
@@ -4,11 +4,20 @@ set -eu
 
 CWD=$(pwd)
 
+if [ -n "${1:+x}" ]; then
+	if [ "$1" = "clean" ]; then
+		echo "Cleaning mbedtls"
+		(cd mbedtls && git reset --hard && git clean -fdx && cd framework && git reset --hard && git clean -fdx)
+		echo "Cleaning zlib"
+		(cd zlib && git reset --hard && git clean -fdx)
+	fi
+fi
+
 echo "Compiling mbedtls"
 (cd mbedtls && CFLAGS="-fPIC -I$CWD/src/main/c -DMBEDTLS_USER_CONFIG_FILE='<mbedtls_custom_config.h>'" make CC=aarch64-linux-gnu-gcc AR=aarch64-linux-gnu-ar no_test)
 
 echo "Compiling zlib"
-(cd zlib && CFLAGS="-fPIC -DNO_GZIP" CC=aarch64-linux-gnu-gcc CHOST=arm64 ./configure --target="aarch64" --static && make CFLAGS="-fPIC -march=armv8-a+crc" CC=aarch64-linux-gnu-gcc AR=aarch64-linux-gnu-ar)
+(cd zlib && CFLAGS="-fPIC -DNO_GZIP" CC=aarch64-linux-gnu-gcc CHOST=arm64 ./configure --static && make CFLAGS="-fPIC -march=armv8-a+crc" CC=aarch64-linux-gnu-gcc AR=aarch64-linux-gnu-ar)
 
 CC="aarch64-linux-gnu-gcc"
 CFLAGS="-c -fPIC -O3 -Wall -Werror -I$JAVA_HOME/include/ -I$JAVA_HOME/include/linux/"
@@ -23,7 +32,7 @@ echo "Linking native-cipher-arm.so"
 $CC $LDFLAGS -o src/main/resources/native-cipher-arm.so shared.o NativeCipherImpl.o mbedtls/library/libmbedcrypto.a
 
 echo "Linking native-compress-arm.so"
-$CC $LDFLAGS -o src/main/resources/native-compress-arm.so shared.o NativeCompressImpl.o zlib/libz.a
+$CC $LDFLAGS -o src/main/resources/native-compress-arm.so shared.o NativeCompressImpl.o zlib/libz-ng.a
 
 echo "Cleaning up"
 rm shared.o NativeCipherImpl.o NativeCompressImpl.o
--- native/compile-native.sh
+++ native/compile-native.sh
@@ -4,6 +4,15 @@ set -eu
 
 CWD=$(pwd)
 
+if [ -n "${1:+x}" ]; then
+	if [ "$1" = "clean" ]; then
+		echo "Cleaning mbedtls"
+		(cd mbedtls && git reset --hard && git clean -fdx && cd framework && git reset --hard && git clean -fdx)
+		echo "Cleaning zlib"
+		(cd zlib && git reset --hard && git clean -fdx)
+	fi
+fi
+
 echo "Compiling mbedtls"
 (cd mbedtls && CFLAGS="-fPIC -I$CWD/src/main/c -DMBEDTLS_USER_CONFIG_FILE='<mbedtls_custom_config.h>'" make no_test)
 
@@ -23,7 +32,7 @@ echo "Linking native-cipher.so"
 $CC $LDFLAGS -o src/main/resources/native-cipher.so shared.o NativeCipherImpl.o mbedtls/library/libmbedcrypto.a
 
 echo "Linking native-compress.so"
-$CC $LDFLAGS -o src/main/resources/native-compress.so shared.o NativeCompressImpl.o zlib/libz.a
+$CC $LDFLAGS -o src/main/resources/native-compress.so shared.o NativeCompressImpl.o zlib/libz-ng.a
 
 echo "Cleaning up"
 rm shared.o NativeCipherImpl.o NativeCompressImpl.o
